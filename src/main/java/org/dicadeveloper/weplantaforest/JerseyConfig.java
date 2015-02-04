60665873e2908d8d5cf068f0ef9f1001daa710b6
Sebastian Froehlich <sfroehlich@datameer.com>
2015-02-04 15:46:29 +0100

    * simplify JerseyConfig on continuous development: just use the specific package which contains all endpoints

--- src/main/java/org/dicadeveloper/weplantaforest/JerseyConfig.java
+++ src/main/java/org/dicadeveloper/weplantaforest/JerseyConfig.java
@@ -1,7 +1,6 @@
 package org.dicadeveloper.weplantaforest;
 
 import org.codehaus.jackson.jaxrs.JacksonJsonProvider;
-import org.dicadeveloper.weplantaforest.endpoints.TreeEndpoint;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.stereotype.Component;
 
@@ -10,7 +9,6 @@ public class JerseyConfig extends ResourceConfig {
 
     public JerseyConfig() {
         register(JacksonJsonProvider.class);
-        register(TreeEndpoint.class);
+        packages("org.dicadeveloper.weplantaforest.endpoints");
     }
-
 }
