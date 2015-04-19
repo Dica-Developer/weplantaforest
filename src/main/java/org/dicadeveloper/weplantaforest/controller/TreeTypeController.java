dde6fd252c09d77f083cdf338aef3f18ca5d368c
Sebastian Froehlich <sfroehlich@datameer.com>
2015-04-19 16:37:53 +0200

    #18 pagination for TreeTypeController

--- src/main/java/org/dicadeveloper/weplantaforest/controller/TreeTypeController.java
+++ src/main/java/org/dicadeveloper/weplantaforest/controller/TreeTypeController.java
@@ -1,14 +1,18 @@
 package org.dicadeveloper.weplantaforest.controller;
 
-import java.util.List;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 import javax.ws.rs.core.MediaType;
 
 import org.dicadeveloper.weplantaforest.PATHS;
 import org.dicadeveloper.weplantaforest.persist.dto.TreeTypeDto;
 import org.dicadeveloper.weplantaforest.services.TreeTypeService;
+import org.dicadeveloper.weplantaforest.util.UtilConstants;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.ExposesResourceFor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
 import org.springframework.http.HttpEntity;
@@ -19,21 +23,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
-
 @RestController
-@ExposesResourceFor(TreeTypeDto.class)
 public class TreeTypeController {
 
     @Autowired
     private TreeTypeService _treeTypeService;
 
-    @RequestMapping(value = PATHS.PATH_TREE_TYPES, method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON })
-    public HttpEntity<Resources<Resource<TreeTypeDto>>> getTreeTypes() {
-        List<TreeTypeDto> treeTypes = _treeTypeService.findAll();
+    @RequestMapping(value = PATHS.PATH_TREE_TYPES + "/", method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON })
+    public HttpEntity<Resources<Resource<TreeTypeDto>>> getTreeTypes(@PageableDefault(size = UtilConstants.DEFAULT_RETURN_RECORD_COUNT, page = 0, sort = { "_name" }) Pageable pageable) {
+        Page<TreeTypeDto> treeTypes = _treeTypeService.findAll(pageable);
         Resources<Resource<TreeTypeDto>> treeTypeResources = Resources.wrap(treeTypes);
-        treeTypeResources.add(linkTo(methodOn(TreeTypeController.class).getTreeTypes()).withSelfRel());
+        treeTypeResources.add(linkTo(methodOn(TreeTypeController.class).getTreeTypes(pageable)).withSelfRel());
         return new ResponseEntity<Resources<Resource<TreeTypeDto>>>(treeTypeResources, HttpStatus.OK);
     }
 
@@ -45,7 +45,7 @@ public class TreeTypeController {
         TreeTypeDto treeType = _treeTypeService.findOne(id);
         Resource<TreeTypeDto> treeTypeResource = new Resource(treeType);
         treeTypeResource.add(linkTo(methodOn(TreeTypeController.class).getTreeType(id)).withSelfRel());
-        treeTypeResource.add(linkTo(methodOn(TreeTypeController.class).getTreeTypes()).withRel("parent"));
+        treeTypeResource.add(linkTo(methodOn(TreeTypeController.class).getTreeTypes(null)).withRel("parent"));
         return new ResponseEntity<Resource<TreeTypeDto>>(treeTypeResource, HttpStatus.OK);
     }
 
