7435622d1f94ed2e9678f1a9e1d0625ad867ebca
Jörg Weber <weber.joerg.h@gmail.com>
2015-06-22 16:14:34 +0200

    * add site map to footer

--- src/main/webapp/client/components/footer/footer.controller.js
+++ src/main/webapp/client/components/footer/footer.controller.js
@@ -1,4 +1,30 @@
 angular.module('IPAT')
-  .controller('FooterCtrl', function () {
+  .controller('FooterCtrl', function ($scope) {
     'use strict';
+
+    $scope.sitemap = {
+      'LINK.PLANTINGS': {
+        'LINK.PLANTINGS': 'main',
+        'LINK.PROJECT_ACREAGES': 'main',
+        'LINK.CERTIFICATE': 'main',
+        'LINK.VOUCHER': 'main'
+      },
+      'LINK.LEAGUE_TABLES': {
+        'LINK.ALL': 'main',
+        'LINK.PRIVATE': 'main',
+        'LINK.COMMERCIAL': 'main',
+        'LINK.NON_PROFIT_ORG': 'main',
+        'LINK.SCHOOLS': 'main',
+        'LINK.TEAMS': 'main'
+      },
+      'LINK.COMPANY': {
+        'LINK.IDEA': 'main',
+        'LINK.CODEX': 'main',
+        'LINK.TEAM': 'main',
+        'LINK.PARTNERS': 'main',
+        'LINK.FINANCIALS': 'main',
+        'LINK.JOBS': 'main',
+        'LINK.PRESS': 'main'
+      }
+    };
   });
