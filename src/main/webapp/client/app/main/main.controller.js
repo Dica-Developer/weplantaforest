62e661cc0ebe982fa69375b069a309ab2a1988b6
Jörg Weber <weber.joerg.h@gmail.com>
2015-06-22 13:07:10 +0200

    * add slides to main page

--- src/main/webapp/client/app/main/main.controller.js
+++ src/main/webapp/client/app/main/main.controller.js
@@ -1,5 +1,23 @@
 angular.module('IPAT')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope) {
     'use strict';
+
+    $scope.slides = [
+      {
+        'image': 'http://lorempixel.com/1140/400/nature/4',
+        'title': 'MAIN.SLIDES.1.TITLE',
+        'text': 'MAIN.SLIDES.1.TEXT'
+      },
+      {
+        'image': 'http://lorempixel.com/1140/400/nature/8',
+        'title': 'MAIN.SLIDES.2.TITLE',
+        'text': 'MAIN.SLIDES.2.TEXT'
+      },
+      {
+        'image': 'http://lorempixel.com/1140/400/nature/12',
+        'title': 'MAIN.SLIDES.3.TITLE',
+        'text': 'MAIN.SLIDES.3.TEXT'
+      }
+    ];
 
   });
