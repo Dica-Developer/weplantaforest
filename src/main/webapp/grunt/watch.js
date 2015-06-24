47ef04307d1cbbf2f6cfaa8ff6de74fa2699a112
Jörg Weber <weber.joerg.h@gmail.com>
2015-06-25 00:56:06 +0200

    * remove live reload option of watch task

--- src/main/webapp/grunt/watch.js
+++ src/main/webapp/grunt/watch.js
@@ -39,18 +39,5 @@ module.exports = {
   },
   gruntfile: {
     files: ['Gruntfile.js']
-  },
-  livereload: {
-    files: [
-      '{.tmp,<%= yeoman.client %>}/{app,components}/**/*.css',
-      '{.tmp,<%= yeoman.client %>}/{app,components}/**/*.html',
-      '{.tmp,<%= yeoman.client %>}/{app,components}/**/*.js',
-      '!{.tmp,<%= yeoman.client %>}{app,components}/**/*.spec.js',
-      '!{.tmp,<%= yeoman.client %>}/{app,components}/**/*.mock.js',
-      '<%= yeoman.client %>/assets/images/{,*//*}*.{png,jpg,jpeg,gif,webp,svg}'
-    ],
-    options: {
-      livereload: true
-    }
   }
 };
