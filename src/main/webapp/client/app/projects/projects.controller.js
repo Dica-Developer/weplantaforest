1862ecf35e74396a2fd411ceb6bb91ef9950828a
Jörg Weber <weber.joerg.h@gmail.com>
2015-06-29 00:37:37 +0200

    * add progress bar to projects teaser

--- src/main/webapp/client/app/projects/projects.controller.js
+++ src/main/webapp/client/app/projects/projects.controller.js
@@ -8,21 +8,32 @@ angular.module('IPAT')
         'name': 'Berlin Mittelheide',
         'pictureLink': 'https://placeholdit.imgix.net/~text?txtsize=26&w=350&h=350&txttrack=0&txt=Berlin Mittelheide',
         'teaser': 'PROJECTS.13.TEASER',
-        'text': 'PROJECTS.13.TEXT'
+        'text': 'PROJECTS.13.TEXT',
+        'total': 18000,
+        'current': 1608
       },
       {
         'id': 12,
         'name': 'Wallendorf II',
         'pictureLink': 'https://placeholdit.imgix.net/~text?txtsize=26&w=350&h=350&txttrack=0&txt=Wallendorf II',
         'teaser': 'PROJECTS.12.TEASER',
-        'text': 'PROJECTS.12.TEXT'
+        'text': 'PROJECTS.12.TEXT',
+        'total': 12400,
+        'current': 11742
       },
       {
         'id': 11,
         'name': 'Deponie Wallendorf I',
         'pictureLink': 'https://placeholdit.imgix.net/~text?txtsize=26&w=350&h=350&txttrack=0&txt=Deponie Wallendorf I',
         'teaser': 'PROJECTS.11.TEASER',
-        'text': 'PROJECTS.11.TEXT'
+        'text': 'PROJECTS.11.TEXT',
+        'total': 6200,
+        'current': 6200
       }
     ];
+
+    $scope.getPercentage = function (total, current) {
+      var percentage = (100 * current / total);
+      return Math.ceil(percentage);
+    };
   });
