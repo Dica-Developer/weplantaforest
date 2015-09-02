8256f9560375788d93d17c9b95a7f5e8f6a9cb21
Martin Schaaf <mschaaf@datameer.com>
2015-09-02 15:57:50 +0200

    * use live data in project overview page instead of hardcoded projects

--- src/main/webapp/client/app/projects/projects.controller.js
+++ src/main/webapp/client/app/projects/projects.controller.js
@@ -1,36 +1,19 @@
 angular.module('IPAT')
-  .controller('ProjectsCtrl', function ($scope) {
+  .controller('ProjectsCtrl', function ($scope, ProjectApi) {
     'use strict';
 
-    $scope.projects = [
-      {
-        'id': 13,
-        'name': 'Berlin Mittelheide',
-        'pictureLink': 'https://placeholdit.imgix.net/~text?txtsize=26&w=350&h=350&txttrack=0&txt=Berlin Mittelheide',
-        'teaser': 'PROJECTS.13.TEASER',
-        'text': 'PROJECTS.13.TEXT',
-        'total': 18000,
-        'current': 1608
-      },
-      {
-        'id': 12,
-        'name': 'Wallendorf II',
-        'pictureLink': 'https://placeholdit.imgix.net/~text?txtsize=26&w=350&h=350&txttrack=0&txt=Wallendorf II',
-        'teaser': 'PROJECTS.12.TEASER',
-        'text': 'PROJECTS.12.TEXT',
-        'total': 12400,
-        'current': 11742
-      },
-      {
-        'id': 11,
-        'name': 'Deponie Wallendorf I',
-        'pictureLink': 'https://placeholdit.imgix.net/~text?txtsize=26&w=350&h=350&txttrack=0&txt=Deponie Wallendorf I',
-        'teaser': 'PROJECTS.11.TEASER',
-        'text': 'PROJECTS.11.TEXT',
-        'total': 6200,
-        'current': 6200
+    $scope.projects = [];
+
+    window.ProjectApi = ProjectApi
+    ProjectApi.query().then(function(response) {
+      if (200 === response.status) {
+          $scope.projects = response.data._embedded.projectDtoes;
+      } else {
+        console.log("error occured");
       }
-    ];
+    }).catch(function() {
+      console.log("error occured");
+    });
 
     $scope.getPercentage = function (total, current) {
       var percentage = (100 * current / total);
