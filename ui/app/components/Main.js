f10d1d943e74c2fb5d234b02b9da99b896f0d784
Martin Schaaf <mschaaf@datameer.com>
2016-05-02 16:07:47 +0200

    * adjust formatting

--- ui/app/components/Main.js
+++ ui/app/components/Main.js
@@ -10,36 +10,39 @@ class Application extends React.Component {
 
 class Co2Bar extends React.Component {
 
-    constructor() {
-        super();
-        this.state = {co2: 0, treesCount: 0.0};
-    }
+  constructor() {
+    super();
+    this.state = {co2: 0, treesCount: 0.0};
+  }
 
-    componentDidMount() {
-        var that = this;
-        axios.get('http://localhost:8081/reports/co2').then(function(co2) {
-            that.setState(co2.data);
-        });
-        window.setInterval(function () {
-            axios.get('http://localhost:8081/reports/co2').then(function(co2) {
-                that.setState(co2.data);
-            })
-        }, 10000);
-    }
+  componentDidMount() {
+    var that = this;
+    axios.get('http://localhost:8081/reports/co2').then(function(co2) {
+      that.setState(co2.data);
+    });
+    window.setInterval(function () {
+      axios.get('http://localhost:8081/reports/co2').then(function(co2) {
+        that.setState(co2.data);
+      })
+    }, 10000);
+  }
 
-    render() {
-        return (<div><div className="col-md-6"><h3>{this.state.co2} <small>t CO2 gebunden</small></h3></div><div className="col-md-6"><h3>{this.state.treesCount} <small>Bäume gepflanzt</small></h3></div></div>);
-    }
+  render() {
+    return (<div><div className="col-md-6"><h3>{this.state.co2} <small>t CO2 gebunden</small></h3></div><div className="col-md-6"><h3>{this.state.treesCount} <small>Bäume gepflanzt</small></h3></div></div>);
+  }
 }
 
 ReactDOM.render(<Co2Bar/>, document.getElementById('co2Bar'));
 /*ReactDOM.render((
-     <Router history={browserHistory}>
-         <Route path="/" component={App}>
-             <Route path="plant" component={Plant}>
-         </Route>
-         <Route path="*" component={NoMatch}/>
-         </Route>
-     </Router>
-), document.getElementById('test'))
-*/
+  <Router history={browserHistory}>
+  <Route path="/" component={App}>
+  <Route path="plant" component={Plant}>
+  </Route>
+  <Route path="*" component={NoMatch}/>
+  </Route>
+  </Router>
+  ), document.getElementById('test'))
+  */
+
+/* vim: set softtabstop=2:shiftwidth=2:expandtab */ 
+
