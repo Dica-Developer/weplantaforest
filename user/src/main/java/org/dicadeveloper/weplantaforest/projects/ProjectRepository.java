2019df6485ab7a4ba06ab4439599d416339775c1
Gabor <Gabor@foe.de>
2016-05-04 17:04:10 -0700

    added query to find project by name

--- user/src/main/java/org/dicadeveloper/weplantaforest/projects/ProjectRepository.java
+++ user/src/main/java/org/dicadeveloper/weplantaforest/projects/ProjectRepository.java
@@ -4,12 +4,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Repository("projectRepository")
 public interface ProjectRepository extends CrudRepository<Project, Long> {
 
+    @Query
+    public Project findByName(@Param("name") String name);
+    
     @Query(value = "SELECT p FROM Project p where p.shopActive=true")
     @Transactional(readOnly = true)
     public Page<Project> active(Pageable pageable);
