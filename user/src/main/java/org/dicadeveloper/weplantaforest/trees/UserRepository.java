51f142b4d17662d5d2923d21ae50852baf005d99
Gabor <Gabor@foe.de>
2016-05-04 17:05:45 -0700

    removed Query-String, not necessary

--- user/src/main/java/org/dicadeveloper/weplantaforest/trees/UserRepository.java
+++ user/src/main/java/org/dicadeveloper/weplantaforest/trees/UserRepository.java
@@ -6,8 +6,6 @@ import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends CrudRepository<User, Long> {
 
-    public final static String FIND_BY_NAME_QUERY = "SELECT user FROM User user WHERE user.name = :name";
-
-    @Query(FIND_BY_NAME_QUERY)
+    @Query
     public User findByName(@Param("name") String name);
 }
