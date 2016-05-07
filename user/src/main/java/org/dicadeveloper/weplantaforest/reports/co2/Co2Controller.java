d7972e37598b7cf64391109f04e062e569635097
Gabor <Gabor@foe.de>
2016-05-06 18:34:05 -0700

    added 10 minute cache to co2Controller and treeRankings

--- user/src/main/java/org/dicadeveloper/weplantaforest/reports/co2/Co2Controller.java
+++ user/src/main/java/org/dicadeveloper/weplantaforest/reports/co2/Co2Controller.java
@@ -1,6 +1,8 @@
 package org.dicadeveloper.weplantaforest.reports.co2;
 
+import org.dicadeveloper.weplantaforest.CacheConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +16,7 @@ public class Co2Controller {
 
     private @NonNull Co2Repository _co2Repository;
 
+    @Cacheable(value = CacheConfiguration.TEN_MIUTE_CACHE)
     @RequestMapping(value = "/reports/co2", method = RequestMethod.GET)
     public Co2Data getAmount() {
         return _co2Repository.getAllTreesAndCo2Saving(System.currentTimeMillis());
--- user/src/main/java/org/dicadeveloper/weplantaforest/reports/rankings/RankingController.java
+++ user/src/main/java/org/dicadeveloper/weplantaforest/reports/rankings/RankingController.java
@@ -1,6 +1,8 @@
 package org.dicadeveloper.weplantaforest.reports.rankings;
 
+import org.dicadeveloper.weplantaforest.CacheConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +18,8 @@ import lombok.RequiredArgsConstructor;
 public class RankingController {
 
     private @NonNull RankingRepository _rankingRepository;
-
+    
+    @Cacheable(value = CacheConfiguration.TEN_MIUTE_CACHE)
     @RequestMapping(value = "/ranking/bestUser/{pageNr}/{pageSize}", method = RequestMethod.GET)
     public Page<TreeRankedUserData> getBestUser(@PathVariable int pageNr, @PathVariable int pageSize) {
         return _rankingRepository.getBestUser(System.currentTimeMillis(), new PageRequest(pageNr, pageSize));
@@ -27,6 +30,7 @@ public class RankingController {
         return _rankingRepository.getLastCreatedUser(new PageRequest(pageNr, pageSize));
     }
 
+    @Cacheable(value = CacheConfiguration.TEN_MIUTE_CACHE)
     @RequestMapping(value = "/ranking/bestOrgType/{organizationType}/{pageNr}/{pageSize}", method = RequestMethod.GET)
     public Page<TreeRankedUserData> getBestUserFromOrganizationType(@PathVariable int organizationType, @PathVariable int pageNr, @PathVariable int pageSize) {
         return _rankingRepository.getBestUserFromOrganizationType(System.currentTimeMillis(), organizationType, new PageRequest(pageNr, pageSize));
@@ -37,6 +41,7 @@ public class RankingController {
         return _rankingRepository.getLastPlantedTrees(new PageRequest(pageNr, pageSize));
     }
     
+    @Cacheable(value = CacheConfiguration.TEN_MIUTE_CACHE)
     @RequestMapping(value = "/ranking/bestTeam/{pageNr}/{pageSize}", method = RequestMethod.GET)
     public Page<TreeRankedUserData> getBestTeams(@PathVariable int pageNr, @PathVariable int pageSize) {
         return _rankingRepository.getBestTeams(System.currentTimeMillis(),new PageRequest(pageNr, pageSize));
