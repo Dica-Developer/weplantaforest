7f2ccac6eaf04293167e92e23eae12af33d489bd
Gabor <Gabor@foe.de>
2016-05-06 18:32:24 -0700

    added caching mechanism to the application
    *created 10-minute cache

---
+++ user/src/main/java/org/dicadeveloper/weplantaforest/CacheConfiguration.java
@@ -0,0 +1,31 @@
+package org.dicadeveloper.weplantaforest;
+
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.guava.GuavaCache;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.google.common.cache.CacheBuilder;
+
+@Configuration
+public class CacheConfiguration {
+
+    public final static String TEN_MIUTE_CACHE = "ten_minute_cache";
+
+    @Bean
+    public CacheManager cacheManager() {
+        SimpleCacheManager cacheManager = new SimpleCacheManager();
+
+        cacheManager.setCaches(Arrays.asList(buildTenMinuteCache()));
+        return cacheManager;
+    }
+
+    private GuavaCache buildTenMinuteCache() {
+        return new GuavaCache(TEN_MIUTE_CACHE, CacheBuilder.newBuilder().expireAfterWrite(10, TimeUnit.MINUTES).build());
+    }
+
+}
--- user/src/main/java/org/dicadeveloper/weplantaforest/WeplantaforestApplication.java
+++ user/src/main/java/org/dicadeveloper/weplantaforest/WeplantaforestApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.hateoas.config.EnableEntityLinks;
 
 @SpringBootApplication
 @EnableEntityLinks
 @EntityScan(basePackageClasses = { WeplantaforestApplication.class, Jsr310JpaConverters.class })
+@EnableCaching
 public class WeplantaforestApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
