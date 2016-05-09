dd3fe1c909543ecb470abcf80aa8d2b1fc13b155
Gabor <Gabor@foe.de>
2016-05-09 13:43:52 -0700

    added query to count already planted trees by a project-article

--- user/src/main/java/org/dicadeveloper/weplantaforest/trees/TreeRepository.java
+++ user/src/main/java/org/dicadeveloper/weplantaforest/trees/TreeRepository.java
@@ -1,5 +1,6 @@
 package org.dicadeveloper.weplantaforest.trees;
 
+import org.dicadeveloper.weplantaforest.projects.ProjectArticle;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -12,7 +13,12 @@ public interface TreeRepository extends CrudRepository<Tree, Long> {
 
     public final static String FIND_TREETYPE_ID_BY_TREE_ID_QUERY = "SELECT tree.treeType.id  FROM Tree tree WHERE tree.id = :id";
 
+    public final static String COUNT_ALREADY_PLANTED_TREES_BY_PROJECTARTICLE = "SELECT sum(tree.amount)  FROM Tree tree WHERE tree.projectArticle = :projectArticle";
+
     @Query(value = FIND_TREETYPE_ID_BY_TREE_ID_QUERY)
     @Transactional(readOnly = true)
     public Long findTreeTypeIdByTreeId(@Param("id") Long treeId);
+
+    @Query(COUNT_ALREADY_PLANTED_TREES_BY_PROJECTARTICLE)
+    public Long countAlreadyPlantedTreesByProjectArticle(@Param("projectArticle") ProjectArticle projectArticle);
 }
---
+++ user/src/test/java/org/dicadeveloper/weplantaforest/trees/TreeRepositoryIntegrationTest.java
@@ -0,0 +1,73 @@
+package org.dicadeveloper.weplantaforest.trees;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.dicadeveloper.weplantaforest.WeplantaforestApplication;
+import org.dicadeveloper.weplantaforest.projects.ProjectArticle;
+import org.dicadeveloper.weplantaforest.projects.ProjectArticleRepository;
+import org.dicadeveloper.weplantaforest.projects.ProjectRepository;
+import org.dicadeveloper.weplantaforest.testsupport.CleanDbRule;
+import org.dicadeveloper.weplantaforest.testsupport.DbInjecter;
+import org.dicadeveloper.weplantaforest.treetypes.TreeTypeRepository;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = WeplantaforestApplication.class)
+@IntegrationTest({ "spring.profiles.active=test" })
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
+public class TreeRepositoryIntegrationTest {
+
+    @Rule
+    @Autowired
+    public CleanDbRule _cleanDbRule;
+
+    @Autowired
+    public DbInjecter _dbInjecter;
+
+    @Autowired
+    private TreeRepository _treeRepository;
+
+    @Autowired
+    private ProjectArticleRepository _projectArticleRepository;
+
+    @Autowired
+    private ProjectRepository _projectRepository;
+
+    @Autowired
+    private TreeTypeRepository _treeTypeRepository;
+
+    @Test
+    public void testGetPlantedTreesFromProjectArticle() {
+        long timeOfPlanting = System.currentTimeMillis();
+
+        _dbInjecter.injectTreeType("wood", "this is a wood", 0.5);
+        _dbInjecter.injectTreeType("big wood", "this is a big wood", 0.5);
+
+        _dbInjecter.injectUser("Adam");
+        _dbInjecter.injectUser("Bert");
+
+        _dbInjecter.injectProject("Project", "Adam", "adam's project", true, 0, 0);
+
+        _dbInjecter.injectPlantArticle("wood", "Project", 3.0);
+        _dbInjecter.injectPlantArticle("big wood", "Project", 3.0);
+
+        _dbInjecter.injectTreeToProject("wood", "Bert", 5, timeOfPlanting, "Project");
+        _dbInjecter.injectTreeToProject("wood", "Bert", 5, timeOfPlanting, "Project");
+        _dbInjecter.injectTreeToProject("big wood", "Bert", 5, timeOfPlanting, "Project");
+
+        ProjectArticle projectArticle = _projectArticleRepository.findByProjectAndTreeType(
+                _projectRepository.findByName("Project"), _treeTypeRepository.findByName("wood"));
+
+        Long plantedTrees = _treeRepository.countAlreadyPlantedTreesByProjectArticle(projectArticle);
+
+        assertThat(plantedTrees).isEqualTo(10);
+    }
+}
