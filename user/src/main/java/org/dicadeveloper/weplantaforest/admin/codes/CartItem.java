5c399df1c66d2bc5f2f0631b2074f820d2b2f5d5
Gabor <Gabor@foe.de>
2016-05-10 08:49:36 -0700

    Edited entity CartItem
    *replaced getter and setter methods with lombok annotations
    *renamed variables - removed the "_" in front of them
    *included default values for BigDecimals directly to the declaration
    *commented out TreeType, seems not to be necessary(look at the comment)
    and also caused some problems while creating a cartItem

--- user/src/main/java/org/dicadeveloper/weplantaforest/admin/codes/CartItem.java
+++ user/src/main/java/org/dicadeveloper/weplantaforest/admin/codes/CartItem.java
@@ -10,14 +10,16 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
 
-import org.dicadeveloper.weplantaforest.treetypes.TreeType;
 import org.springframework.hateoas.Identifiable;
 
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 @NoArgsConstructor
 public class CartItem implements Identifiable<Long> {
 
@@ -32,112 +34,36 @@ public class CartItem implements Identifiable<Long> {
     // private String _plantName;
 
     // TODO: implement
-    private Long _treeId;
+    @Column(name = "_treeId")
+    private Long treeId;
 
-    // TODO: remove
-    // a real reference?
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "_treeType_treeTypeId")
-    private TreeType _treeType;
+    // // TODO: remove
+    // // a real reference?
+    // @OneToOne(fetch = FetchType.LAZY)
+    // @JoinColumn(name = "_treeType_treeTypeId")
+    // private TreeType tt;
 
-    private int _amount;
+    @Column(name = "_amount")
+    private int amount;
 
-    private Long _plantArticleId;
+    @Column(name = "_plantArticleId")
+    private Long plantArticleId;
 
-    @Column(precision = 7, scale = 2)
-    private BigDecimal _basePricePerPiece;
+    @Column(name = "_basePricePerPiece", precision = 7, scale = 2)
+    private BigDecimal basePricePerPiece = BigDecimal.ZERO;
 
-    @Column(precision = 7, scale = 2)
-    private BigDecimal _scontoPerPiece;
+    @Column(name = "_scontoPerPiece", precision = 7, scale = 2)
+    private BigDecimal scontoPerPiece = BigDecimal.ZERO;
 
-    @Column(precision = 7, scale = 2)
-    private BigDecimal _fundingPerPiece;
+    @Column(name = "_fundingPerPiece", precision = 7, scale = 2)
+    private BigDecimal fundingPerPiece = BigDecimal.ZERO;
 
-    @Column(precision = 7, scale = 2)
-    private BigDecimal _totalPrice;
+    @Column(name = "_totalPrice", precision = 7, scale = 2)
+    private BigDecimal totalPrice = BigDecimal.ZERO;
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "_cart__cartId")
     private Cart cart;
-
-    public Long getId() {
-        return id;
-    }
-
-    public Cart getCart() {
-        return cart;
-    }
-
-    public void setCart(final Cart cart) {
-        this.cart = cart;
-    }
-
-    // TODO: remove from here
-    public TreeType getTreeType() {
-        return _treeType;
-    }
-
-    public void setTreeType(final TreeType treeType) {
-        _treeType = treeType;
-    }
-
-    // to here
-
-    public BigDecimal getBasePricePerPiece() {
-        return _basePricePerPiece == null ? BigDecimal.ZERO : _basePricePerPiece;
-    }
-
-    public void setBasePricePerPiece(final BigDecimal basePricePerPiece) {
-        _basePricePerPiece = basePricePerPiece;
-    }
-
-    public BigDecimal getScontoPerPiece() {
-        return _scontoPerPiece == null ? BigDecimal.ZERO : _scontoPerPiece;
-    }
-
-    public void setScontoPerPiece(final BigDecimal scontoPerPiece) {
-        _scontoPerPiece = scontoPerPiece;
-    }
-
-    public BigDecimal getFundingPerPiece() {
-        return _fundingPerPiece == null ? BigDecimal.ZERO : _fundingPerPiece;
-    }
-
-    public void setFundingPerPiece(final BigDecimal fundingPerPiece) {
-        _fundingPerPiece = fundingPerPiece;
-    }
-
-    public BigDecimal getTotalPrice() {
-        return _totalPrice == null ? BigDecimal.ZERO : _totalPrice;
-    }
-
-    public void setTotalPrice(final BigDecimal totalPrice) {
-        _totalPrice = totalPrice;
-    }
-
-    public int getAmount() {
-        return _amount;
-    }
-
-    public void setAmount(final int amount) {
-        _amount = amount;
-    }
-
-    public Long getPlantArticleId() {
-        return _plantArticleId;
-    }
-
-    public void setPlantArticleId(final Long plantArticleId) {
-        _plantArticleId = plantArticleId;
-    }
-
-    public void setTreeId(final Long treeId) {
-        _treeId = treeId;
-    }
-
-    public Long getTreeId() {
-        return _treeId;
-    }
 
     @Override
     public String toString() {
---
+++ user/src/test/java/org/dicadeveloper/weplantaforest/admin/codes/CartItemTest.java
@@ -0,0 +1,60 @@
+package org.dicadeveloper.weplantaforest.admin.codes;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.math.BigDecimal;
+
+import org.dicadeveloper.weplantaforest.WeplantaforestApplication;
+import org.dicadeveloper.weplantaforest.testsupport.CleanDbRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@SpringApplicationConfiguration(classes = WeplantaforestApplication.class)
+@IntegrationTest({ "spring.profiles.active=test" })
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
+public class CartItemTest {
+
+    @Rule
+    @Autowired
+    public CleanDbRule _cleanDbRule;
+
+    @Test
+    public void testBigDecimalObjectsNotNull() {
+        CartItem cartItem = new CartItem();    
+
+        assertThat(cartItem.getBasePricePerPiece()).isNotNull();
+        assertThat(cartItem.getBasePricePerPiece()).isEqualTo(BigDecimal.ZERO);
+        assertThat(cartItem.getScontoPerPiece()).isNotNull();
+        assertThat(cartItem.getScontoPerPiece()).isEqualTo(BigDecimal.ZERO);
+        assertThat(cartItem.getFundingPerPiece()).isNotNull();
+        assertThat(cartItem.getFundingPerPiece()).isEqualTo(BigDecimal.ZERO);
+        assertThat(cartItem.getTotalPrice()).isNotNull();
+        assertThat(cartItem.getTotalPrice()).isEqualTo(BigDecimal.ZERO);
+    }
+    
+    @Test
+    public void testBigDecimalObjectsNoDefaultWhenSetAnotherValue() {
+        CartItem cartItem = new CartItem();    
+        
+        cartItem.setBasePricePerPiece(BigDecimal.TEN);
+        cartItem.setFundingPerPiece(BigDecimal.TEN);
+        cartItem.setScontoPerPiece(BigDecimal.TEN);
+        cartItem.setTotalPrice(BigDecimal.TEN);
+
+        assertThat(cartItem.getBasePricePerPiece()).isEqualTo(BigDecimal.TEN);
+        assertThat(cartItem.getScontoPerPiece()).isEqualTo(BigDecimal.TEN);
+        assertThat(cartItem.getFundingPerPiece()).isEqualTo(BigDecimal.TEN);
+        assertThat(cartItem.getTotalPrice()).isEqualTo(BigDecimal.TEN);
+    }
+    
+}
