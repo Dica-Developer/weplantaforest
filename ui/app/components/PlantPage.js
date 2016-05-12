a49bc2419d813ccebf7bf0c0d8ca1cc94232228e
Martin Schaaf <mschaaf@datameer.com>
2016-05-12 18:43:47 +0200

    * use new url for fetching pre calculated carts to a maximum amount a user want to spend

--- ui/app/components/PlantPage.js
+++ ui/app/components/PlantPage.js
@@ -3,32 +3,31 @@ import NavBar from './NavBar';
 import Footer from './Footer';
 import Bootstrap from 'bootstrap';
 import Accounting from 'accounting';
+import axios from 'axios';
 
 class PaymentBar extends Component {
 
   constructor() {
     super();
-    this.state = {price: 500, trees: [{count: 5, treeType: {name: "Buche"}}]};
+    this.state = {actualPrice: 500, projects: []};
   }
 
   toggleButtonState(amount) {
-    switch (amount) {
-      case 500:
-        this.setState({price: 500, trees: [{count: 5, treeType: {name: "Buche"}}]});
-        break;
-      case 1000:
-        this.setState({price: 1000, trees: [{count: 5, treeType: {name: "Buche"}}, {count: 3, treeType: {name: "Erle"}}, {count: 2, treeType: {name: "Eiche"}}]});
-        break;
-      case 1500:
-        this.setState({price: 1500, trees: [{count: 5, treeType: {name: "Buche"}}, {count: 3, treeType: {name: "Erle"}}, {count: 2, treeType: {name: "Eiche"}}, {count: 10, treeType: {name: "Fichte"}}]});
-        break;
-    }
+    var that = this;
+    axios.get('http://localhost:8081/plantProposal/' + amount).then(function(response) {
+      that.setState(response.data);
+    });
   }
 
   componentDidMount() {
+    var that = this;
+    axios.get('http://localhost:8081/plantProposal/500').then(function(response) {
+      that.setState(response.data);
+    });
   }
 
   render() {
+        var that = this;
         return (<div className="container">
           Hier kannst Du Bäume pflanzen. Du wählst aus wieviel Bäume oder für wieviel Geld wir für Dich Bäume pflanzen.
           <br/>
@@ -45,21 +44,43 @@ class PaymentBar extends Component {
           </div>
           <br />
           <br />
-          <ul className="list-group">
-          {this.state.trees.map(function (item) {
-            return (<li className="list-group-item">
-              <span className="badge">{item.count}</span>
-              {item.treeType.name}
-            </li>);
+          {Object.keys(that.state.projects).map(function (projectName) {
+            return (<div className="panel panel-default">
+              <div className="panel-heading">{projectName}</div>
+                <div className="panel-body">
+                  <ul className="list-group">
+                  {Object.keys(that.state.projects[projectName].plantItems).map(function (treeName) {
+                    return (<li className="list-group-item">
+                      <span className="badge">{that.state.projects[projectName].plantItems[treeName].amount}</span>
+                      {treeName}
+                    </li>)
+                  })}
+                  </ul>
+                </div>
+              </div>)
           })}
-          </ul>
           <button type="button" className="btn btn-group-justified btn-default btn-lg">Nehme ich für {(() => {
-            return Accounting.formatMoney(this.state.price / 100, { thousand: '.', decimal: ',', symbol: '€',  format: '%v %s' });
+            return Accounting.formatMoney(this.state.actualPrice / 100, { thousand: '.', decimal: ',', symbol: '€',  format: '%v %s' });
           })()}</button>
           <br />
         </div>)
   }
 }
+         /* {this.state.projects.entries.each(function(projectName, trees) {
+            return (<div class="panel panel-default">
+              <div class="panel-heading">{projectName}</div>
+                <div class="panel-body">
+                  <ul className="list-group">
+                  for (var i = 0; i < trees.entries.length; i++) {
+                    <li className="list-group-item">
+                      <span className="badge">{tress.entries[i].value.amount}</span>
+                      {item.entries[i].key}
+                    </li>
+                  }
+                  </ul>
+                </div>
+              </div>)
+          })}*/
 
 export default class PlantPage extends Component {
   render() {
