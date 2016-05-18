29a2281ba1b9baa3c01d6aae99de04f627df4836
Gab0rB <gabor_barath@yahoo.de>
2016-05-18 11:10:35 -0700

    removed entities Article and Paragraph and their repositories

--- user/src/main/java/org/dicadeveloper/weplantaforest/articles/ParagraphRepository.java
+++
@@ -1,7 +0,0 @@
-package org.dicadeveloper.weplantaforest.articles;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ParagraphRepository extends CrudRepository<Paragraph, Long>{
-
-}
