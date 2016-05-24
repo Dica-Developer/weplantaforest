73b4eea7454fe8ff6433316e63adcb5b74645112
Gab0rB <gabor_barath@yahoo.de>
2016-05-24 22:10:59 +0200

    changed order by created on to DESC

--- article-manager/src/main/java/org/dicadeveloper/weplantaforest/reports/articles/ArticleDataRepository.java
+++ article-manager/src/main/java/org/dicadeveloper/weplantaforest/reports/articles/ArticleDataRepository.java
@@ -11,7 +11,7 @@ import org.springframework.data.repository.query.Param;
 public interface ArticleDataRepository extends CrudRepository<Article, Long> {
 
     public final static String FIND_ARTICLES_BY_TYPE_QUERY = "SELECT new org.dicadeveloper.weplantaforest.reports.articles.ArticleData(article.title, article.intro)"
-            + "FROM Article article WHERE article.articleType = :articleType ORDER BY article.createdOn ";
+            + "FROM Article article WHERE article.articleType = :articleType ORDER BY article.createdOn DESC";
 
     public final static String FIND_PARAGRAPHS_BY_ARTICLE_TITLE_QUERY = "SELECT new org.dicadeveloper.weplantaforest.reports.articles.ArticleContentData(paragraph.title, paragraph.text)"
             + " FROM Paragraph paragraph where paragraph.article.title = :articleTitle";
