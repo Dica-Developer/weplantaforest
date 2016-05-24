b75aaf8351befc686f2e2a3ecf261657a09673e4
Gab0rB <gabor_barath@yahoo.de>
2016-05-24 17:10:23 +0200

    added class HashUtil and SwitchCoverageTester
    *added method to HashUtil, which generates a String with the same
    hashCode, but isn't the same String
    *SwitchCoverageTester will test the missing branches of a switch, where
    you need a String with the same hash than the hases from the Strings of
    the cases

---
+++ common/src/test/java/org/dicadeveloper/weplantaforest/common/testSupport/HashUtil.java
@@ -0,0 +1,20 @@
+package org.dicadeveloper.weplantaforest.common.testSupport;
+
+public class HashUtil {
+
+    public static String samehash(String s, int level) {
+        if (s.length() < 2)
+            return s;
+        String re;
+        do {
+            String sub2 = s.substring(0, 2);
+            char c0 = sub2.charAt(0);
+            char c1 = sub2.charAt(1);
+            c0 = (char) (c0 + level);
+            c1 = (char) (c1 - 31 * level);
+            String newsub2 = new String(new char[] { c0, c1 });
+            re = newsub2 + s.substring(2);
+        } while (re.equals(s));
+        return re;
+    }
+}
---
+++ user/src/test/java/org/dicadeveloper/weplantaforest/testsupport/SwitchCoverageTester.java
@@ -0,0 +1,60 @@
+package org.dicadeveloper.weplantaforest.testsupport;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+import org.dicadeveloper.weplantaforest.WeplantaforestApplication;
+import org.dicadeveloper.weplantaforest.common.testSupport.CleanDbRule;
+import org.dicadeveloper.weplantaforest.common.testSupport.HashUtil;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@SpringApplicationConfiguration(classes = WeplantaforestApplication.class)
+@IntegrationTest({ "spring.profiles.active=test" })
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
+public class SwitchCoverageTester {
+
+    private MockMvc mockMvc;
+
+    @Rule
+    @Autowired
+    public CleanDbRule _cleanDbRule;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    public DbInjecter _dbInjecter;
+
+    @Before
+    public void setup() {
+        this.mockMvc = webAppContextSetup(this.webApplicationContext).build();
+    }
+
+    @Test
+    public void testGetBestUserFromTimeRangeCoverAllBranches() throws Exception {
+        String sameHashForWeek = HashUtil.samehash("week", 1);
+        String sameHashForYear = HashUtil.samehash("year", 1);
+
+        this.mockMvc.perform(get("/ranking/bestUserFromTimeRange/{range}", sameHashForWeek).accept("application/json"))
+                    .andExpect(status().isOk());
+
+        this.mockMvc.perform(get("/ranking/bestUserFromTimeRange/{range}", sameHashForYear).accept("application/json"))
+                    .andExpect(status().isOk());
+    }
+
+}
