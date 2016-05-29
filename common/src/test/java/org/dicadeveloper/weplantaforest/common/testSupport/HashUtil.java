0e96f07d99d9984b305925020458f2d7999cc196
Gab0rB <gabor_barath@yahoo.de>
2016-05-29 20:21:31 +0200

    added curly braces to if statement and..
    renamed variables to longer, more speaking names

--- common/src/test/java/org/dicadeveloper/weplantaforest/common/testSupport/HashUtil.java
+++ common/src/test/java/org/dicadeveloper/weplantaforest/common/testSupport/HashUtil.java
@@ -2,18 +2,19 @@ package org.dicadeveloper.weplantaforest.common.testSupport;
 
 public class HashUtil {
 
-    public static String samehash(String s, int level) {
-        if (s.length() < 2)
-            return s;
+    public static String samehash(String string, int level) {
+        if (string.length() < 2){
+            return string;            
+        }
 
-        String re;
-        String sub2 = s.substring(0, 2);
+        String stringWithSameHash;
+        String sub2 = string.substring(0, 2);
         char c0 = sub2.charAt(0);
         char c1 = sub2.charAt(1);
         c0 = (char) (c0 + level);
         c1 = (char) (c1 - 31 * level);
         String newsub2 = new String(new char[] { c0, c1 });
-        re = newsub2 + s.substring(2);
-        return re;
+        stringWithSameHash = newsub2 + string.substring(2);
+        return stringWithSameHash;
     }
 }
