0700e0a34121d39363149c66261ea6f693eefddf
Gab0rB <gabor_barath@yahoo.de>
2016-06-01 17:51:06 +0200

    added ImageRepository

---
+++ user/src/main/java/org/dicadeveloper/weplantaforest/image/ImageRepository.java
@@ -0,0 +1,9 @@
+package org.dicadeveloper.weplantaforest.image;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ImageRepository extends CrudRepository<Image, Long> {
+
+   
+    
+}
