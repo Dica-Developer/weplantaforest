7df64933855e0b2c89da472b491ae12d1373557c
Gab0rB <gabor_barath@yahoo.de>
2016-06-06 16:35:32 +0200

    added ordering projects by projectId desc

--- user/src/main/java/org/dicadeveloper/weplantaforest/reports/projects/ProjectReportRepository.java
+++ user/src/main/java/org/dicadeveloper/weplantaforest/reports/projects/ProjectReportRepository.java
@@ -12,7 +12,7 @@ public interface ProjectReportRepository extends CrudRepository<Project, Long> {
     public final static String FIND_ALL_PROJECTS_QUERY = "SELECT new org.dicadeveloper.weplantaforest.reports.projects.ProjectReportData( "
             + "project.id, project.name, project.imageFileName, project.description, project.longitude, project.latitude, SUM(articles.amount), "
             + "COALESCE((SELECT SUM(tree.amount) FROM Tree as tree WHERE tree.projectArticle IN(SELECT article FROM ProjectArticle article WHERE article.project = project)),0), project.shopActive) "
-            + "FROM Project project JOIN project.articles articles WHERE articles.project = project GROUP BY project.name";
+            + "FROM Project project JOIN project.articles articles WHERE articles.project = project GROUP BY project.name ORDER BY project.id DESC";
 
     public final static String FIND_ALL_PROJECTS_COUNT_QUERY = "SELECT count(distinct project.name) from Project project";
 
