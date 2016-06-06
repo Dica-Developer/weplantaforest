23838787e4281a873d136ca0c2a7057565156419
Gab0rB <gabor_barath@yahoo.de>
2016-06-06 11:35:46 +0200

    removed where clause from count query

--- user/src/main/java/org/dicadeveloper/weplantaforest/reports/projects/ProjectReportRepository.java
+++ user/src/main/java/org/dicadeveloper/weplantaforest/reports/projects/ProjectReportRepository.java
@@ -14,7 +14,7 @@ public interface ProjectReportRepository extends CrudRepository<Project, Long> {
             + "COALESCE((SELECT SUM(tree.amount) FROM Tree as tree WHERE tree.projectArticle IN(SELECT article FROM ProjectArticle article WHERE article.project = project)),0), project.shopActive) "
             + "FROM Project project JOIN project.articles articles WHERE articles.project = project GROUP BY project.name";
 
-    public final static String FIND_ALL_PROJECTS_COUNT_QUERY = "SELECT count(distinct project.name) from Project project WHERE project.shopActive = true";
+    public final static String FIND_ALL_PROJECTS_COUNT_QUERY = "SELECT count(distinct project.name) from Project project";
 
     public final static String FIND_POJECT_DATA_BY_NAME = "SELECT new org.dicadeveloper.weplantaforest.reports.projects.ProjectReportData( "
             + "project.id, project.name, project.imageFileName, project.description, project.longitude, project.latitude, SUM(articles.amount), "
