1002f8d71ce1a01365d050a264b842a554a5569c
Gab0rB <gabor_barath@yahoo.de>
2016-06-07 16:09:54 +0200

    corrected query string names

--- user/src/main/java/org/dicadeveloper/weplantaforest/trees/TreeRepository.java
+++ user/src/main/java/org/dicadeveloper/weplantaforest/trees/TreeRepository.java
@@ -17,9 +17,9 @@ public interface TreeRepository extends PagingAndSortingRepository<Tree, Long> {
 
     public final static String COUNT_ALREADY_PLANTED_TREES_BY_PROJECTARTICLE = "SELECT COALESCE(sum(tree.amount), 0)  FROM Tree tree WHERE tree.projectArticle = :projectArticle";
     
-    public final static String FIND_TREES_BYUSER_ID_ID_QUERY = "SELECT tree  FROM Tree tree WHERE tree.owner.id = :ownerId";
+    public final static String FIND_TREES_BY_USER_ID_QUERY = "SELECT tree  FROM Tree tree WHERE tree.owner.id = :ownerId";
     
-    public final static String FIND_TREES_BY_PPROJECT_ID_ID_QUERY = "SELECT tree  FROM Tree tree WHERE tree.projectArticle.project.id = :projectId";
+    public final static String FIND_TREES_BY_PROJECT_ID_QUERY = "SELECT tree  FROM Tree tree WHERE tree.projectArticle.project.id = :projectId";
 
     @Query(value = FIND_TREETYPE_ID_BY_TREE_ID_QUERY)
     @Transactional(readOnly = true)
@@ -28,9 +28,9 @@ public interface TreeRepository extends PagingAndSortingRepository<Tree, Long> {
     @Query(COUNT_ALREADY_PLANTED_TREES_BY_PROJECTARTICLE)
     public Long countAlreadyPlantedTreesByProjectArticle(@Param("projectArticle") ProjectArticle projectArticle);
     
-    @Query(value = FIND_TREES_BYUSER_ID_ID_QUERY)
+    @Query(value = FIND_TREES_BY_USER_ID_QUERY)
     public Page<Tree> findTreesByUserId(@Param("ownerId") Long ownerId, Pageable page);
     
-    @Query(value = FIND_TREES_BY_PPROJECT_ID_ID_QUERY)
+    @Query(value = FIND_TREES_BY_PROJECT_ID_QUERY)
     public Page<Tree> findTreesByProjectId(@Param("projectId") Long projectId, Pageable page);
 }
