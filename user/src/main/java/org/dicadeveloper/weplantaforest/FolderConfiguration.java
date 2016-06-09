5cd25e8e6ea4148abf29a9b9975079af5cf9f592
Gab0rB <gabor_barath@yahoo.de>
2016-06-09 15:37:12 -0700

    added FolderConfiguration
    *in every stage.application file there's now a variable called upload.root
    --> in dev mode it's a relative path to the application root(folder temp -> added to gitignore), in test and production mode there can be added correct absolute paths if it's clear where to save this files(it's now initialized with 'D:')

---
+++ user/src/main/java/org/dicadeveloper/weplantaforest/FolderConfiguration.java
@@ -0,0 +1,34 @@
+package org.dicadeveloper.weplantaforest;
+
+import java.io.File;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+@Configuration
+public class FolderConfiguration {
+
+    @Autowired
+    Environment env;
+
+    public static String UPLOAD_DIR = "/ipat_uploads_user";
+
+    public static String IMAGE_DIR = "/images";
+
+    public static String PROJECT_DIR = "/projects";
+
+    @Bean
+    CommandLineRunner initFolderStructure() {
+        return (String[] args) -> {
+            String topFolder = env.getProperty("upload.root");
+
+            new File(topFolder).mkdir();
+            new File(topFolder + UPLOAD_DIR).mkdir();
+            new File(topFolder + UPLOAD_DIR + IMAGE_DIR).mkdir();
+            new File(topFolder + UPLOAD_DIR + IMAGE_DIR + PROJECT_DIR).mkdir();
+        };
+    }
+}
