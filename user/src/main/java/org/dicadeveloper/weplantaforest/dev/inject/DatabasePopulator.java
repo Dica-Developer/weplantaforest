8bd3a97a1e2ddd74176fbf60d1a8d39a6fb9fb79
Gab0rB <gabor_barath@yahoo.de>
2016-06-15 16:17:01 -0700

    organized import

--- user/src/main/java/org/dicadeveloper/weplantaforest/dev/inject/DatabasePopulator.java
+++ user/src/main/java/org/dicadeveloper/weplantaforest/dev/inject/DatabasePopulator.java
@@ -11,8 +11,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
-import javax.transaction.Transactional;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.dicadeveloper.weplantaforest.FileSystemInjector;
