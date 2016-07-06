5cd9e75612a40adefe5632f3dc767803a9c66284
Gab0rB <gabor_barath@yahoo.de>
2016-07-06 13:40:11 -0700

    added test class for PasswordEncrypter

---
+++ user/src/test/java/org/dicadeveloper/weplantaforest/encrypter/PasswordEncrypterTest.java
@@ -0,0 +1,40 @@
+package org.dicadeveloper.weplantaforest.encrypter;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.dicadeveloper.weplantaforest.WeplantaforestApplication;
+import org.dicadeveloper.weplantaforest.common.testSupport.CleanDbRule;
+import org.dicadeveloper.weplantaforest.encryption.PasswordEncrypter;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@SpringApplicationConfiguration(classes = WeplantaforestApplication.class)
+@IntegrationTest({ "spring.profiles.active=test" })
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
+public class PasswordEncrypterTest {
+
+    @Rule
+    @Autowired
+    public CleanDbRule _cleanDbRule;
+
+    @Autowired
+    private PasswordEncrypter _passwordEncrypter;
+
+    @Test
+    public void testEncryptPassword() {
+        String password = "password";
+
+        String encryptedPassword = _passwordEncrypter.encryptPassword(password);
+        assertThat(encryptedPassword).isEqualTo("W6ph5Mm5Pz8GgiULbPgzG37mj9g=");
+    }
+}
