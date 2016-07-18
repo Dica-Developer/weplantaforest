862e27218f8eb5ebec8c9ebcd4bff3313345523d
Gab0rB <gabor_barath@yahoo.de>
2016-07-18 10:20:36 -0700

    added dummy test class to generate jacocoReport

---
+++ admin/src/test/java/org/dicadeveloper/weplantaforest/admin/DummyTest.java
@@ -0,0 +1,14 @@
+package org.dicadeveloper.weplantaforest.admin;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+public class DummyTest {
+
+    @Test
+    public void testDummy() {
+        assertThat(true).isTrue();
+    }
+
+}
