deebd1fad7029b0a11e35262a38a1c55d9bff3a3
Gab0rB <gabor_barath@yahoo.de>
2016-07-18 10:54:22 -0700

    renamed RestConfigurationAdmin to RestConfiguration

--- admin/src/main/java/org/dicadeveloper/weplantaforest/admin/RestConfiguration.java
+++ admin/src/main/java/org/dicadeveloper/weplantaforest/admin/RestConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
-public class RestConfigurationAdmin {
+public class RestConfiguration {
 
     @Bean
     public CorsFilter corsFilter() {
