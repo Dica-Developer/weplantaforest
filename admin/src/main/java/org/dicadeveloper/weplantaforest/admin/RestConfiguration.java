bb17e6ce14cec1c9dd85c6b9bb9d5dca69aff8bb
Gab0rB <gabor_barath@yahoo.de>
2016-07-18 09:46:11 -0700

    added backend-module admin

---
+++ admin/src/main/java/org/dicadeveloper/weplantaforest/admin/RestConfiguration.java
@@ -0,0 +1,24 @@
+package org.dicadeveloper.weplantaforest.admin;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class RestConfiguration {
+
+    @Bean
+    public CorsFilter corsFilter() {
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:8080");
+        config.addAllowedOrigin("http://localhost:8082");
+        config.addAllowedHeader("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
---
+++ admin/src/main/java/org/dicadeveloper/weplantaforest/admin/WeplantaforestAdminApplication.java
@@ -0,0 +1,20 @@
+package org.dicadeveloper.weplantaforest.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+import org.springframework.hateoas.config.EnableEntityLinks;
+
+@SpringBootApplication
+@EnableEntityLinks
+@EntityScan(basePackageClasses = { WeplantaforestAdminApplication.class, Jsr310JpaConverters.class })
+@EnableCaching
+public class WeplantaforestAdminApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeplantaforestAdminApplication.class, args);
+    }
+}
