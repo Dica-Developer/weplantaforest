5d33ed1003ede000ce8cb560cfa871e740b0cdfa
Gab0rB <gabor_barath@yahoo.de>
2016-07-18 15:05:18 -0700

    added ComponentScan-annotation
    to include files from module common
    *this doesn't with default config, cause of the new top-level package
    "admin"

--- admin/src/main/java/org/dicadeveloper/weplantaforest/admin/WeplantaforestAdminApplication.java
+++ admin/src/main/java/org/dicadeveloper/weplantaforest/admin/WeplantaforestAdminApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.hateoas.config.EnableEntityLinks;
 
 @SpringBootApplication
 @EnableEntityLinks
 @EntityScan(basePackageClasses = { WeplantaforestAdminApplication.class, Jsr310JpaConverters.class })
+@ComponentScan(basePackages = {"org.dicadeveloper.weplantaforest.*"})
 @EnableCaching
 public class WeplantaforestAdminApplication extends SpringBootServletInitializer {
 
