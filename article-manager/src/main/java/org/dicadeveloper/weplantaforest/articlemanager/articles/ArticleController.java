4e42dde54b8a89a5e8cfd1f717726b2599145454
Gab0rB <gabor_barath@yahoo.de>
2016-07-18 17:21:12 -0700

    added methods to upload article and paragraph images

--- article-manager/src/main/java/org/dicadeveloper/weplantaforest/articlemanager/articles/ArticleController.java
+++ article-manager/src/main/java/org/dicadeveloper/weplantaforest/articlemanager/articles/ArticleController.java
@@ -1,5 +1,6 @@
 package org.dicadeveloper.weplantaforest.articlemanager.articles;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,6 +8,7 @@ import javax.transaction.Transactional;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.dicadeveloper.weplantaforest.articlemanager.FileSystemInjector;
 import org.dicadeveloper.weplantaforest.common.image.ImageHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,7 +16,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
@@ -47,6 +51,38 @@ public class ArticleController {
         } catch (Exception e) {
             LOG.error("Error occured while saving article!", e);
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @RequestMapping(value = "/article/upload/image", method = RequestMethod.POST)
+    public ResponseEntity<?> uploadArticleImage(@RequestParam Long articleId, @RequestParam String imgType, @RequestParam("file") MultipartFile file) {
+        String folder = FileSystemInjector.getArticleFolder() + "/" + articleId;
+        String imageName = "main" + "." + imgType;
+        try {
+            imageName = _imageHelper.storeImage(file, folder, imageName);
+            Article articleForImage = _articleRepository.findOne(articleId);
+            articleForImage.setImageFileName(imageName);
+            _articleRepository.save(articleForImage);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (IOException e) {
+            LOG.error("Error occured while saving article!", e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @RequestMapping(value = "/paragraph/upload/image", method = RequestMethod.POST)
+    public ResponseEntity<?> uploadParagraphImage(@RequestParam Long articleId, @RequestParam Long paragraphId, @RequestParam String imgType, @RequestParam("file") MultipartFile file) {
+        String folder = FileSystemInjector.getArticleFolder() + "/" + articleId;
+        String imageName = "paragraph_"+ paragraphId + "." + imgType;
+        try {
+            imageName = _imageHelper.storeImage(file, folder, imageName);
+            Paragraph paragraphForImage = _paragraphRepository.findOne(paragraphId);
+            paragraphForImage.setImageFileName(imageName);
+            _paragraphRepository.save(paragraphForImage);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (IOException e) {
+            LOG.error("Error occured while saving article!", e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
--- article-manager/src/test/java/org/dicadeveloper/weplantaforest/articlemanager/article/ArticleControllerTest.java
+++ article-manager/src/test/java/org/dicadeveloper/weplantaforest/articlemanager/article/ArticleControllerTest.java
@@ -5,9 +5,12 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.dicadeveloper.weplantaforest.articlemanager.FileSystemInjector;
 import org.dicadeveloper.weplantaforest.articlemanager.WeplantaforestArticleManagerApplication;
 import org.dicadeveloper.weplantaforest.articlemanager.articles.Article;
 import org.dicadeveloper.weplantaforest.articlemanager.articles.Article.ArticleType;
@@ -19,6 +22,7 @@ import org.dicadeveloper.weplantaforest.articlemanager.user.UserRepository;
 import org.dicadeveloper.weplantaforest.common.support.Language;
 import org.dicadeveloper.weplantaforest.common.testSupport.CleanDbRule;
 import org.dicadeveloper.weplantaforest.common.testSupport.TestUtil;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -26,11 +30,14 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -66,6 +73,11 @@ public class ArticleControllerTest {
         mockMvc = webAppContextSetup(this.webApplicationContext).build();
     }
 
+    @After
+    public void cleanUp() {
+        TestUtil.deleteFilesInDirectory(new File(FileSystemInjector.getArticleFolder()));
+    }
+
     @Test
     public void testCreateArticle() throws Exception {
         _dbInjecter.injectUser("Adam");
@@ -94,5 +106,52 @@ public class ArticleControllerTest {
 
         assertThat(_articleRepository.count()).isEqualTo(1L);
         assertThat(_paragraphRepository.count()).isEqualTo(1L);
+    }
+
+    @Test
+    public void testAddArticleImage() throws Exception {
+        _dbInjecter.injectUser("manager");
+        _dbInjecter.injectArticle("title", "intro", ArticleType.BLOG, "manager", 1000000L);
+
+        FileInputStream fileInputStream = new FileInputStream("src/test/resources/images/" + "article1.jpg");
+        MockMultipartFile image = new MockMultipartFile("file", fileInputStream);
+
+        MediaType mediaType = new MediaType("multipart", "form-data");
+
+        mockMvc.perform(MockMvcRequestBuilders.fileUpload("/article/upload/image")
+                                              .file(image)
+                                              .contentType(mediaType)
+                                              .param("articleId", "1")
+                                              .param("imgType", "jpg"))
+               .andExpect(status().isOk());
+
+        assertThat(_articleRepository.findOne(1L)
+                                     .getImageFileName()).isEqualTo("main.jpg");
+        TestUtil.deleteFilesInDirectory(new File(FileSystemInjector.getArticleFolder() + "/1"));
+
+    }
+
+    @Test
+    public void testAddParagraphImage() throws Exception {
+        _dbInjecter.injectUser("manager");
+        _dbInjecter.injectArticle("title", "intro", ArticleType.BLOG, "manager", 1000000L);
+        _dbInjecter.injectParagraphToArticle("title", "paragraph title", "paragraph text");
+
+        FileInputStream fileInputStream = new FileInputStream("src/test/resources/images/" + "article1.jpg");
+        MockMultipartFile image = new MockMultipartFile("file", fileInputStream);
+
+        MediaType mediaType = new MediaType("multipart", "form-data");
+
+        mockMvc.perform(MockMvcRequestBuilders.fileUpload("/paragraph/upload/image")
+                                              .file(image)
+                                              .contentType(mediaType)
+                                              .param("articleId", "1")
+                                              .param("paragraphId", "1")
+                                              .param("imgType", "jpg"))
+               .andExpect(status().isOk());
+        assertThat(_paragraphRepository.findOne(1L)
+                .getImageFileName()).isEqualTo("paragraph_1.jpg");
+        
+        TestUtil.deleteFilesInDirectory(new File(FileSystemInjector.getArticleFolder() + "/1"));
     }
 }
