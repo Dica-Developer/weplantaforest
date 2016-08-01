94b546741bde57495d0d70684284ef849387c3b2
Gab0rB <gabor_barath@yahoo.de>
2016-08-01 10:41:02 -0700

    jsonIgnore admin and members to avoid infinite recursion in json-object

--- user/src/main/java/org/dicadeveloper/weplantaforest/admin/codes/Team.java
+++ user/src/main/java/org/dicadeveloper/weplantaforest/admin/codes/Team.java
@@ -17,6 +17,8 @@ import javax.persistence.OneToOne;
 import org.dicadeveloper.weplantaforest.user.User;
 import org.springframework.hateoas.Identifiable;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -40,6 +42,8 @@ public class Team implements Identifiable<Long> {
     private String name;
 
     @OneToMany(mappedBy = "team", fetch = FetchType.EAGER)
+    // necessary to avoid infinite recursion in authenticated user json object 
+    @JsonIgnore
     private List<User> members = new ArrayList<User>();
 
     @Column(/* length = ModifiedMySql5InnoDbDialect.LENGTH_FOR_TEXT_TYPE, */ columnDefinition = "TEXT")
@@ -47,6 +51,8 @@ public class Team implements Identifiable<Long> {
 
     @OneToOne(optional = false)
     @JoinColumn(name = "_admin__userId")
+    // necessary to avoid infinite recursion in authenticated user json object 
+    @JsonIgnore
     private User admin;
 
     public void addMember(final User member) {
