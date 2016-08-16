692badb7804ecd1572f903cc168927f6c3a21d2b
Gab0rB <gabor_barath@yahoo.de>
2016-08-16 10:01:08 -0700

    added tbody tag

--- ui/src/js/components/ButtonBar.js
+++ ui/src/js/components/ButtonBar.js
@@ -1,9 +1,5 @@
-import React, {
-  Component
-} from 'react';
-import {
-  Link
-} from 'react-router';
+import React, {Component} from 'react';
+import {Link} from 'react-router';
 
 export default class ButtonBar extends Component {
 
@@ -15,44 +11,47 @@ export default class ButtonBar extends Component {
   render() {
     return (
       <div className="buttonBar">
-      <table>
-        <tr>
-          <td>
-            <Link to="/" >
-              <img src="/assets/images/Spaten.png" alt="selbst pflanzen" width="25" height="50"/>
-            </Link>
-          </td>
-          <td>
-            <Link to="/plant" >
-              <span className="no-link-deco">SELBST PFLANZEN</span>
-            </Link>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <Link to="/plant" >
-              <img src="/assets/images/Maus.png" alt="online pflanzen" width="50" height="50"/>
-            </Link>
-          </td>
-          <td>
-            <Link to="/plant" >
-              <span className="no-link-deco">ONLINE PFLANZEN</span>
-            </Link>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <Link to="/" >
-              <img src="/assets/images/SCHERE.png" alt="selbst pflanzen" width="50" height="50"/>
-            </Link>
-          </td>
-          <td>
-            <Link to="/plant" >
-              <span className="no-link-deco">BAUMSERVICE</span>
-            </Link>
-          </td>
-        </tr>
-      </table>
-    </div>);
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                <Link to="/">
+                  <img src="/assets/images/Spaten.png" alt="selbst pflanzen" width="25" height="50"/>
+                </Link>
+              </td>
+              <td>
+                <Link to="/plant">
+                  <span className="no-link-deco">SELBST PFLANZEN</span>
+                </Link>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <Link to="/plant">
+                  <img src="/assets/images/Maus.png" alt="online pflanzen" width="50" height="50"/>
+                </Link>
+              </td>
+              <td>
+                <Link to="/plant">
+                  <span className="no-link-deco">ONLINE PFLANZEN</span>
+                </Link>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <Link to="/">
+                  <img src="/assets/images/SCHERE.png" alt="selbst pflanzen" width="50" height="50"/>
+                </Link>
+              </td>
+              <td>
+                <Link to="/plant">
+                  <span className="no-link-deco">BAUMSERVICE</span>
+                </Link>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
   }
 }
