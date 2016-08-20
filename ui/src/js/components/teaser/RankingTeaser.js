edd1bcacf3e05ece0e389e5e5565c8b8fb653cf5
Gab0rB <gabor_barath@yahoo.de>
2016-08-20 15:51:17 -0700

    formatted parameter co2Saved

--- ui/src/js/components/teaser/RankingTeaser.js
+++ ui/src/js/components/teaser/RankingTeaser.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import React, {Component} from 'react';
 import {render} from 'react-dom';
 import {Map, Marker, Popup, TileLayer} from 'react-leaflet';
+import Accounting from 'accounting';
 
 import Boostrap from 'bootstrap';
 
@@ -18,7 +19,7 @@ export default class RankingTeaser extends Component {
           <i>{this.props.title}</i>
         </h2>
         {this.props.content.content.map(function(content) {
-          let co2Rounded = Math.round(content.co2Saved * 1000) / 1000;
+          let co2Rounded = Accounting.formatNumber(content.co2Saved * 1000 / 1000, 3, ".", ",");
           let imageUrl = 'http://localhost:8081/' + imageFolder + '/image/' + content.imageName + '/60/60';
           return (
             <div>
