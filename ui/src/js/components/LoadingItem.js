9c20d578a7c988e7890f9180c8d8f8f2842f1e53
Gab0rB <gabor_barath@yahoo.de>
2016-08-22 10:32:12 -0700

    created LoadingItem and included in mainPage->teaser

---
+++ ui/src/js/components/LoadingItem.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+import {Map, Marker, Popup, TileLayer} from 'react-leaflet';
+import {Link} from 'react-router';
+
+import Boostrap from 'bootstrap';
+
+export default class LoadingItem extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div className={"col-md-4 loadingItem " + this.props.background}>
+        <h2>Daten werden geladen...</h2>
+        <div>
+          <span className="glyphicon glyphicon-refresh spinning"></span>
+        </div>
+      </div>
+    );
+  }
+}
+
+/* vim: set softtabstop=2:shiftwidth=2:expandtab */
--- ui/src/js/components/Teaser.js
+++ ui/src/js/components/Teaser.js
@@ -5,71 +5,28 @@ import axios from 'axios';
 import ArticleTeaser from '../components/teaser/ArticleTeaser';
 import ProjectTeaser from '../components/teaser/ProjectTeaser';
 import RankingTeaser from '../components/teaser/RankingTeaser';
+import LoadingItem from '../components/LoadingItem';
 
 export default class Teaser extends Component {
   constructor() {
     super();
     this.state = {
       projects: {
-        content: [
-          {
-            projectId: '',
-            projectName: '',
-            description: '',
-            longitude: 0,
-            latitude: 0,
-            amountOfMaximumTreesToPlant: '',
-            amountOfPlantedTrees: ''
-          }, {
-            projectId: '',
-            projectName: '',
-            description: '',
-            longitude: 0,
-            latitude: 0,
-            amountOfMaximumTreesToPlant: '',
-            amountOfPlantedTrees: ''
-          }
-        ]
+        content: []
       },
       teaser: {
-        content: [
-          {
-            id: '',
-            title: '',
-            imageFileName: '',
-            intro: ''
-          }, {
-            id: '',
-            title: '',
-            imageFileName: '',
-            intro: ''
-          }
-        ]
+        content: []
       },
       bestUserRanking: {
-        content: [
-          {
-            name: '',
-            amount: 0,
-            co2Saved: 0.0,
-            imageName: ''
-          }
-        ]
+        content: []
       },
       bestCompanyRanking: {
-        content: [
-          {
-            name: '',
-            amount: 0,
-            co2Saved: 0.0,
-            imageName: ''
-          }
-        ]
+        content: []
       }
     };
   }
 
-  componentDidMount() {
+  componentWillMount() {
     var that = this;
 
     axios.get('http://localhost:8081/reports/activeProjects?page=0&size=2').then(function(response) {
@@ -130,17 +87,59 @@ export default class Teaser extends Component {
   }
 
   render() {
+    var project1 = [];
+    var project2 = [];
+
+    var article1 = [];
+    var article2 = [];
+
+    var bestUser = [];
+    var bestCompany = [];
+
+    if (this.state.projects.content[0]) {
+      project1.push(<ProjectTeaser content={this.state.projects.content[0]}/>);
+    } else {
+      project1.push(<LoadingItem/>);
+    }
+    if (this.state.teaser.content[0]) {
+      article1.push(<ArticleTeaser content={this.state.teaser.content[0]}/>);
+    } else {
+      article1.push(<LoadingItem/>);
+    }
+    if (this.state.projects.content[1]) {
+      project2.push(<ProjectTeaser content={this.state.projects.content[1]}/>);
+    } else {
+      project2.push(<LoadingItem/>);
+    }
+    if (this.state.teaser.content[1]) {
+      article2.push(<ArticleTeaser content={this.state.teaser.content[1]}/>);
+    } else {
+      article2.push(<LoadingItem/>);
+    }
+
+    if (this.state.bestUserRanking.content[0]) {
+      bestUser.push(<RankingTeaser title="Beste Pflanzer" content={this.state.bestUserRanking} background="lightBlue" imageFolder="user"/>);
+    } else {
+      bestUser.push(<LoadingItem background="lightBlue"/>);
+    }
+
+    if (this.state.bestCompanyRanking.content[0]) {
+      bestCompany.push(<RankingTeaser title="Beste Firmen" content={this.state.bestCompanyRanking} background="violett" imageFolder="user"/>);
+    } else {
+      bestCompany.push(<LoadingItem background="violett"/>);
+    }
+
     return (
       <div>
-        <div className="row teaser is-table-row">
-          <ProjectTeaser content={this.state.projects.content[0]}/>
-          <ArticleTeaser content={this.state.teaser.content[0]}/>
-          <RankingTeaser title="Beste Pflanzer" content={this.state.bestUserRanking} background="lightBlue" imageFolder="user"/>
+        <div className="row teaser">
+          {project1}
+          {article1}
+          {bestUser}
         </div>
-        <div className="row teaser is-table-row">
-          <ProjectTeaser content={this.state.projects.content[1]}/>
-          <ArticleTeaser content={this.state.teaser.content[1]}/>
-          <RankingTeaser title="Beste Firmen" content={this.state.bestCompanyRanking} background="violett" imageFolder="user"/>
+        <div className="row teaser">
+          {project2}
+          {article2}
+          {bestCompany}
         </div>
       </div>
     );
