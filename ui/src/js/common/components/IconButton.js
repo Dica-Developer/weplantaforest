cdd0a657a306da1cd9aa01e0ed8c79e3a27baa17
Gab0rB <gabor_barath@yahoo.de>
2016-09-15 12:32:02 -0700

    created IconButton component

---
+++ ui/src/js/common/components/IconButton.js
@@ -0,0 +1,33 @@
+import React, {
+  Component
+} from 'react';
+import {
+  render
+} from 'react-dom';
+import Boostrap from 'bootstrap';
+
+require("./iconButton.less");
+
+export default class IconButton extends Component {
+
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className="iconButton">
+      <a role="button" onClick={this.props.onClick.bind(this)}>
+        <div>
+          <span className={("glyphicon " + this.props.glyphIcon)} aria-hidden="true"></span>
+          <span>
+          {this.props.text}
+          </span>
+        </div>
+      </a>
+      </div>
+    );
+  }
+}
+
+/* vim: set softtabstop=2:shiftwidth=2:expandtab */
