857ed6da0464cb27d2adc316d35cb21b9b98a349
Gab0rB <gabor_barath@yahoo.de>
2016-09-19 18:06:22 -0700

    created Notification module

---
+++ ui/src/js/common/components/Notification.js
@@ -0,0 +1,44 @@
+import React, {
+  Component
+} from 'react';
+import {
+  render
+} from 'react-dom';
+import NotificationSystem from 'react-notification-system';
+import Boostrap from 'bootstrap';
+
+export default class Notification extends Component {
+
+  addNotification(title, message, type){
+    this.refs.notificationSystem.addNotification({
+      title: title,
+      position: 'tc',
+      autoDismiss: 0,
+      message: message,
+      level: type
+    });
+  }
+
+  render() {
+    var style = {
+      Containers: {
+        DefaultStyle: {
+          zIndex: 11000
+        },
+        tc: {
+          top: '50%',
+          bottom: 'auto',
+          margin: '0 auto',
+          left: '50%'
+        }
+      }
+    };
+    return (
+      <div>
+        <NotificationSystem ref="notificationSystem" style={style}/>
+      </div>
+    );
+  }
+}
+
+/* vim: set softtabstop=2:shiftwidth=2:expandtab */
