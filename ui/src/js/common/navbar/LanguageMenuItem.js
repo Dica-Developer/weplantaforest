c07dbfee7c3760b3186e1d840078282b2d4717b3
Gab0rB <gabor_barath@yahoo.de>
2016-09-21 19:28:57 -0700

    created Language select Menu Item

---
+++ ui/src/js/common/navbar/LanguageMenuItem.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router';
+
+export default class LanguageMenuItem extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="language-menu-item">
+        <a role="button" className={(this.props.language == 'DEUTSCH') ? "selected" : ""} onClick={()=>{this.props.updateLanguage('DEUTSCH')}}>DEU</a>
+        <a role="button" className={(this.props.language == 'ENGLISH') ? "selected" : ""}  onClick={()=>{this.props.updateLanguage('ENGLISH')}}>ENG</a>
+      </div>
+    );
+  }
+}
