54ae451abefab3598eed1bff82cd72b6e588b205
Gab0rB <gabor_barath@yahoo.de>
2016-10-07 13:32:22 -0700

    added padding to description, shortened to 300 letters and not escaping embedded html code

--- ui/src/js/main/ArticleTeaser.js
+++ ui/src/js/main/ArticleTeaser.js
@@ -13,7 +13,7 @@ export default class ArticleTeaser extends Component {
   render() {
     let imageUrl = '';
     if (this.props.content.id != 0) {
-      imageUrl = 'http://localhost:8083/article/image/' + this.props.content.id + '/' + this.props.content.imageFileName + '/380/380';
+      imageUrl = 'http://localhost:8082/article/image/' + this.props.content.id + '/' + this.props.content.imageFileName + '/380/380';
     }
 
     var shortIntro = getTextForSelectedLanguage(this.props.content.intro);
@@ -29,12 +29,14 @@ export default class ArticleTeaser extends Component {
         <h3>
           <i>{this.props.content.title}</i>
         </h3>
-        <p dangerouslySetInnerHTML={{
-          __html: shortIntro
-        }}/>
-        <Link className="more" to={`/projects/` + this.props.content.projectName}>
-          <i>(mehr)</i>
-        </Link>
+        <div className="description">
+          <p dangerouslySetInnerHTML={{
+            __html: shortIntro
+          }}/>
+          <Link className="more" to={`/projects/` + this.props.content.projectName}>
+            <i>(mehr)</i>
+          </Link>
+        </div>
       </div>
     );
   }
