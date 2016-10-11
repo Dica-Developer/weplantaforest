3aa4b02f1e318716372c45494ab19f7fd6daae7e
Gab0rB <gabor_barath@yahoo.de>
2016-10-11 15:49:52 -0700

    added notification, if amount is higher than available tree amount

--- ui/src/js/planting/customPlantPage/Article.js
+++ ui/src/js/planting/customPlantPage/Article.js
@@ -12,6 +12,7 @@ import {
   browserHistory
 } from 'react-router';
 
+import Notification from '../../common/components/Notification';
 import NavBar from '../../common/navbar/NavBar';
 import Header from '../../common/header/Header';
 import Footer from '../../common/Footer';
@@ -23,37 +24,42 @@ export default class Article extends Component {
 
   constructor() {
     super();
-    this.state={
+    this.state = {
       amount: null
     }
   }
 
-  getAmount(){
+  getAmount() {
     return this.state.amount;
   }
 
-  getPrice(){
+  getPrice() {
     return this.props.article.price.priceAsLong;
   }
 
-  updateAmount(event){
-    this.state.amount = event.target.value;
-    this.props.updatePrice();
-    this.forceUpdate();
-  }
+  updateAmount(event) {
+    if (event.target.value <= (this.props.article.amount - this.props.article.alreadyPlanted)) {
+        this.state.amount = event.target.value;
+        this.props.updatePrice();
+        this.forceUpdate();
+      } else {
+        this.refs.notification.addNotification('Zu viele Bäume!', 'Von diesem Baum sind leider nur noch ' + (this.props.article.amount - this.props.article.alreadyPlanted) + ' übrig.', 'error');
+        this.refs.amountInput.value = this.state.amount;
+      }
+    }
 
-  render() {
-    let imageUrl = 'http://localhost:8081/treeType/image/' + this.props.article.treeType.imageFile + '/60/60';
-    return (
-      <div className="article">
-        <div>
+    render() {
+      let imageUrl = 'http://localhost:8081/treeType/image/' + this.props.article.treeType.imageFile + '/60/60';
+      return (
+        <div className="article">
+        <div className="image">
           <img src={imageUrl} />
         </div>
         <div>
           <p><span className="bold uppercase">{this.props.article.treeType.name}</span><br/>Stk.&nbsp;<span className="bold">{Accounting.formatNumber(this.props.article.price.priceAsLong/100, 2, ".", ",")}&nbsp;€</span></p>
         </div>
         <div>
-          <input type="text" value={this.state.value} onChange={this.updateAmount.bind(this)}/>
+          <input ref="amountInput" type="text" value={this.state.value} onChange={this.updateAmount.bind(this)}/>
           &nbsp;/&nbsp;{this.props.article.amount - this.props.article.alreadyPlanted}
         </div>
         <div>
@@ -62,9 +68,10 @@ export default class Article extends Component {
         <div>
           <p className="bold">{Accounting.formatNumber(this.state.amount * this.props.article.price.priceAsLong / 100, 2, ".", ",")}&nbsp;€</p>
         </div>
+        <Notification ref="notification" />
       </div>
-    );
+      );
+    }
   }
-}
 
-/* vim: set softtabstop=2:shiftwidth=2:expandtab */
+  /* vim: set softtabstop=2:shiftwidth=2:expandtab */
