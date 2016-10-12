17683159d7a0918dbf128d9a029f127d10681e52
Gab0rB <gabor_barath@yahoo.de>
2016-10-12 09:35:24 -0700

    changed plantBagItem divs to fixed width

--- ui/src/js/plantBag/PlantBagItem.js
+++ ui/src/js/plantBag/PlantBagItem.js
@@ -1,9 +1,5 @@
-import React, {
-  Component
-} from 'react';
-import {
-  render
-} from 'react-dom';
+import React, {Component} from 'react';
+import {render} from 'react-dom';
 import Accounting from 'accounting';
 import Boostrap from 'bootstrap';
 
@@ -21,21 +17,23 @@ export default class PlantBagItem extends Component {
     let imageUrl = 'http://localhost:8081/treeType/image/' + this.props.plantBagitem.imageFile + '/60/60';
     return (
       <div className="plantBagItem">
-        <div>
-          <img src={imageUrl} />
+        <div className="image">
+          <img src={imageUrl}/>
         </div>
-        <div>
-          <p><span className="bold uppercase">{this.props.plantItemName}</span><br/>Stk.&nbsp;<span className="bold">{Accounting.formatNumber(this.props.plantBagitem.price/100, 2, ".", ",")}&nbsp;€</span></p>
+        <div className="treeTypeInfo">
+          <p>
+            <span className="bold uppercase">{this.props.plantItemName}</span><br/>Stk.&nbsp;<span className="bold">{Accounting.formatNumber(this.props.plantBagitem.price / 100, 2, ".", ",")}&nbsp;€</span>
+          </p>
         </div>
-        <div>
+        <div className="customizer">
           <IconButton glyphIcon="glyphicon-minus" onClick={this.props.decreasePlantBagItem.bind(this)}/>
-          <p className="bold">{this.props.plantBagitem.amount}</p>
+          <span className="bold">{this.props.plantBagitem.amount}</span>
           <IconButton glyphIcon="glyphicon-plus" onClick={this.props.increasePlantBagItem.bind(this)}/>
         </div>
-        <div>
+        <div className="result-arrow">
           <span className="glyphicon glyphicon-share-alt" aria-hidden="true"></span>
         </div>
-        <div>
+        <div className="result">
           <p className="bold">{Accounting.formatNumber(this.props.plantBagitem.amount * this.props.plantBagitem.price / 100, 2, ".", ",")}&nbsp;€</p>
           <IconButton glyphIcon="glyphicon-trash" onClick={this.props.removePlantBagItem.bind(this)}/>
         </div>
