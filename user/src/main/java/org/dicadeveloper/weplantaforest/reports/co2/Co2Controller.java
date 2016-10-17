c7e23d38a67c01fa65e1a2f6db822cfe09a14b8b
Gab0rB <gabor_barath@yahoo.de>
2016-10-17 10:21:31 +0200

    changed return type of Co2Controller to ResponseEntity

--- user/src/main/java/org/dicadeveloper/weplantaforest/reports/co2/Co2Controller.java
+++ user/src/main/java/org/dicadeveloper/weplantaforest/reports/co2/Co2Controller.java
@@ -15,17 +15,18 @@ import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@RequiredArgsConstructor(onConstructor = @__(@Autowired) )
 public class Co2Controller {
 
     private @NonNull Co2Repository _co2Repository;
 
     @Cacheable(value = CacheConfiguration.TEN_MINUTE_CACHE)
     @RequestMapping(value = Uris.REPORT_CO2, method = RequestMethod.GET)
-    public Co2Data getAmount() {
-        return _co2Repository.getAllTreesAndCo2Saving(System.currentTimeMillis());
+    public ResponseEntity<?> getAmount() {
+        Co2Data co2Data = _co2Repository.getAllTreesAndCo2Saving(System.currentTimeMillis());
+        return new ResponseEntity<>(co2Data, HttpStatus.OK);
     }
-    
+
     @RequestMapping(value = Uris.REPORT_CO2_FOR_USER, method = RequestMethod.GET)
     public ResponseEntity<?> getTreesAndCo2ForUser(@RequestParam("userName") String userName) {
         Co2Data co2Data = _co2Repository.getAllTreesAndCo2SavingForUserName(System.currentTimeMillis(), userName);
