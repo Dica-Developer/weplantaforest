55d1567e5ac03aa16f13707d08ae72a3c02b52f4
Gab0rB <gabor_barath@yahoo.de>
2016-10-17 10:05:37 +0200

    removed ImageController package

--- user/src/main/java/org/dicadeveloper/weplantaforest/image/ImageRepository.java
+++
@@ -1,9 +0,0 @@
-package org.dicadeveloper.weplantaforest.image;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ImageRepository extends CrudRepository<Image, Long> {
-
-   
-    
-}
