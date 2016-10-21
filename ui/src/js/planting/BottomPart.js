5ee8c25adb9f6f0e8ec8b19b1abcf8a59a7c0f84
Gab0rB <gabor_barath@yahoo.de>
2016-10-21 11:30:21 +0200

    styled PlantPage for smaller devices

--- ui/src/js/planting/BottomPart.js
+++ ui/src/js/planting/BottomPart.js
@@ -1,16 +1,9 @@
-import React, {
-  Component
-} from 'react';
-import {
-  render
-} from 'react-dom';
+import React, {Component} from 'react';
+import {render} from 'react-dom';
 import Boostrap from 'bootstrap';
 import axios from 'axios';
 import Accounting from 'accounting';
-import {
-  Link,
-  browserHistory
-} from 'react-router';
+import {Link, browserHistory} from 'react-router';
 
 import ImageButton from '../common/components/ImageButton';
 import IconButton from '../common/components/IconButton';
@@ -25,23 +18,18 @@ export default class BottomPart extends Component {
 
   render() {
     return (
-        <div className="bottomPart">
-          <table className="bottomTable">
-            <tbody>
-              <tr>
-                <td></td>
-                <td>
-                  <span>GESAMT:&nbsp;{Accounting.formatNumber(this.props.overallPrice / 100, 2, ".", ",")}&nbsp;€</span>
-                </td>
-                <td>
-                  <ImageButton text="AB IN MEINEN<br/>PFLANZKORB" onClick={this.props.updatePlantBag.bind(this)} imagePath="/assets/images/Schubkarre_braun.png" imageWidth="72" imageHeight="40"/>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+      <div className="bottomPart">
+        <div className="button-and-price">
+          <div className="price">
+            <span>GESAMT:&nbsp;{Accounting.formatNumber(this.props.overallPrice / 100, 2, ".", ",")}&nbsp;€</span>
+          </div>
+          <div className="plantBagButton">
+            <ImageButton text="AB IN MEINEN<br/>PFLANZKORB" onClick={this.props.updatePlantBag.bind(this)} imagePath="/assets/images/Schubkarre_braun.png" imageWidth="72" imageHeight="40"/>
+          </div>
+        </div>
         <div className="line">Dir stehen Flächen zur Verfügung und Du hast Interesse daran, dass I Plant a Tree diese bepflanzt?
           <br/>
-            Hier kanns Du eine&nbsp;
+          Hier kanns Du eine&nbsp;
           <IconButton glyphIcon="glyphicon-forward" text="PROJEKTFLÄCHE ANBIETEN" onClick={this.switchToOfferProjectPage.bind(this)}/>
         </div>
       </div>
