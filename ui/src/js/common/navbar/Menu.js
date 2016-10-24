40ddd1056c7c567dd511d0e372ef3d41c8dc79a0
Gab0rB <gabor_barath@yahoo.de>
2016-10-24 14:35:37 +0200

    added onClick listener when clicking on document to hide the menu

--- ui/src/js/common/navbar/Menu.js
+++ ui/src/js/common/navbar/Menu.js
@@ -6,13 +6,20 @@ export default class Menu extends Component {
     this.state = {
       visible: false
     };
+    this.hide = this.hide.bind(this);
   }
 
   show() {
     this.setState({visible: true});
+    document.addEventListener("click", this.hide);
+  }
+
+  test() {
+    this.setState({visible: false});
   }
 
   hide() {
+    document.removeEventListener("click", this.hide);
     this.setState({visible: false});
   }
 
@@ -22,11 +29,11 @@ export default class Menu extends Component {
         <div className={(this.state.visible
           ? "visible "
           : "notvisible ") + this.props.alignment}>
-        <div className="closeMenu">
-          <button onClick={this.hide.bind(this)}>
-            X
-          </button>
-        </div>
+          <div className="closeMenu">
+            <button onClick={this.hide.bind(this)}>
+              X
+            </button>
+          </div>
           {this.props.children}</div>
       </div>
     );
