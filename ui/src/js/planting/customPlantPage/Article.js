10b5ac9053585626681a80eecd60e76bcd0fb483
Gab0rB <gabor_barath@yahoo.de>
2016-11-05 13:29:39 +0100

    added scaling animation, when changing amount of trees on custom plantPage
    * also moved css for scaling animation to main.less

--- ui/src/js/planting/customPlantPage/Article.js
+++ ui/src/js/planting/customPlantPage/Article.js
@@ -1,16 +1,9 @@
-import React, {
-  Component
-} from 'react';
-import {
-  render
-} from 'react-dom';
+import React, {Component} from 'react';
+import {render} from 'react-dom';
 import Boostrap from 'bootstrap';
 import axios from 'axios';
 import Accounting from 'accounting';
-import {
-  Link,
-  browserHistory
-} from 'react-router';
+import {Link, browserHistory} from 'react-router';
 import {getTextForSelectedLanguage} from '../../common/language/LanguageHelper';
 
 import Notification from '../../common/components/Notification';
@@ -28,8 +21,24 @@ export default class Article extends Component {
   constructor() {
     super();
     this.state = {
-      amount: null
+      amount: null,
+      scaleResult: false
     }
+    this.scalingDone = this.scalingDone.bind(this);
+  }
+
+  scalingDone() {
+    this.setState({scaleResult: false});
+  }
+
+  componentWillUnmount() {
+    const elm = this.refs.result;
+    elm.removeEventListener('animationend', this.scalingDone);
+  }
+
+  componentDidMount() {
+    const elm = this.refs.result;
+    elm.addEventListener('animationend', this.scalingDone);
   }
 
   getAmount() {
@@ -42,39 +51,42 @@ export default class Article extends Component {
 
   updateAmount(event) {
     if (event.target.value <= (this.props.article.amount - this.props.article.alreadyPlanted)) {
-        this.state.amount = event.target.value;
-        this.props.updatePrice();
-        this.forceUpdate();
-      } else {
-        this.refs.notification.addNotification('Zu viele Bäume!', 'Von diesem Baum sind leider nur noch ' + (this.props.article.amount - this.props.article.alreadyPlanted) + ' übrig.', 'error');
-        this.refs.amountInput.value = this.state.amount;
-      }
+      this.state.amount = event.target.value;
+      this.props.updatePrice();
+      this.forceUpdate();
+    } else {
+      this.refs.notification.addNotification('Zu viele Bäume!', 'Von diesem Baum sind leider nur noch ' + (this.props.article.amount - this.props.article.alreadyPlanted) + ' übrig.', 'error');
+      this.refs.amountInput.value = this.state.amount;
     }
+    this.setState({scaleResult: true});
+  }
 
-    render() {
-      let imageUrl = 'http://localhost:8081/treeType/image/' + this.props.article.treeType.imageFile + '/60/60';
-      return (
-        <div className="article">
+  render() {
+    let imageUrl = 'http://localhost:8081/treeType/image/' + this.props.article.treeType.imageFile + '/60/60';
+    return (
+      <div className="article">
         <div className="image">
-          <img src={imageUrl} />
+          <img src={imageUrl}/>
         </div>
         <div>
-          <p><span className="bold uppercase">{getTextForSelectedLanguage(this.props.article.treeType.name)}</span><br/>Stk.&nbsp;<span className="bold">{Accounting.formatNumber(this.props.article.price.priceAsLong/100, 2, ".", ",")}&nbsp;€</span></p>
+          <p>
+            <span className="bold uppercase">{getTextForSelectedLanguage(this.props.article.treeType.name)}</span><br/>Stk.&nbsp;<span className="bold">{Accounting.formatNumber(this.props.article.price.priceAsLong / 100, 2, ".", ",")}&nbsp;€</span>
+          </p>
         </div>
         <div>
           <input ref="amountInput" type="text" value={this.state.value} onChange={this.updateAmount.bind(this)}/>
           &nbsp;/&nbsp;{this.props.article.amount - this.props.article.alreadyPlanted}
         </div>
+        <div></div>
         <div>
-          <span className="glyphicon glyphicon-share-alt" aria-hidden="true"></span>
-        </div>
-        <div>
-          <p className="bold">{Accounting.formatNumber(this.state.amount * this.props.article.price.priceAsLong / 100, 2, ".", ",")}&nbsp;€</p>
-        </div>
-        <Notification ref="notification" />
+          <p ref="result" className={(this.state.scaleResult
+            ? 'scaleResult'
+            : ' ') + " bold"}>{Accounting.formatNumber(this.state.amount * this.props.article.price.priceAsLong / 100, 2, ".", ",")}&nbsp;€</p>
+        </div >
+        <Notification ref="notification"/>
       </div>
-      );
-    }
+    );
   }
+}
 
-  /* vim: set softtabstop=2:shiftwidth=2:expandtab */
+/* vim: set softtabstop=2:shiftwidth=2:expandtab */
