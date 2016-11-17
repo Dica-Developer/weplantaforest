9a2b287ac5402704e7b68e14279cb4896780a850
Gab0rB <gabor_barath@yahoo.de>
2016-11-17 17:40:28 +0100

    changed cleanUpInitialCarts to delete whole cart objects

--- user/src/main/java/org/dicadeveloper/weplantaforest/scheduling/ScheduledTasks.java
+++ user/src/main/java/org/dicadeveloper/weplantaforest/scheduling/ScheduledTasks.java
@@ -1,14 +1,11 @@
 package org.dicadeveloper.weplantaforest.scheduling;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.dicadeveloper.weplantaforest.cart.Cart;
-import org.dicadeveloper.weplantaforest.cart.CartItem;
 import org.dicadeveloper.weplantaforest.cart.CartRepository;
-import org.dicadeveloper.weplantaforest.trees.Tree;
 import org.dicadeveloper.weplantaforest.trees.TreeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -22,36 +19,24 @@ public class ScheduledTasks {
     private static final Log LOG = LogFactory.getLog(ScheduledTasks.class.getName());
 
     private final static long DAY_IN_MILLISECONDS = 86400000;
-    
+
     private final static long FOUR_HOURS_IN_MILLISECONDS = 14400000;
-    
+
     @Autowired
     private @NonNull CartRepository _cartRepository;
+
     @Autowired
     private @NonNull TreeRepository _treeRepository;
-//        
-//    @Scheduled(fixedRate = DAY_IN_MILLISECONDS)
-//    public void checkAbos() {
-//        
-//    }
-    
-    
+    //
+    // @Scheduled(fixedRate = DAY_IN_MILLISECONDS)
+    // public void checkAbos() {
+    //
+    // }
+
     @Scheduled(fixedRate = FOUR_HOURS_IN_MILLISECONDS)
-    private void cleanUpInitialCarts(){
+    private void cleanUpInitialCarts() {
         List<Cart> carts = _cartRepository.findInitialCartsOlderThanFourHours(System.currentTimeMillis());
-        List<Tree> treesToDelete = new ArrayList<>();
-        for(Cart cart : carts){
-            for(CartItem cartItem : cart.getCartItems()){
-                if(cartItem.getTree() != null){
-                    treesToDelete.add(cartItem.getTree());
-                    cartItem.removeTree();                                 
-                }
-            }
-            _cartRepository.save(cart);
-            for(Tree treeToDelete : treesToDelete){
-                _treeRepository.delete(treeToDelete);
-            }
-        }
-        LOG.info("cleaned up initial carts");
+        _cartRepository.delete(carts);
+        LOG.info("deleted initial carts(" + carts.size() + ") and their trees");
     }
 }
