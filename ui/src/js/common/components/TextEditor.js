4094cd420fcf09849983d6903276238e32b7d9b5
Gab0rB <gabor_barath@yahoo.de>
2016-12-02 17:21:02 +0100

    created test editor component based on tinymce editor

---
+++ ui/src/js/common/components/TextEditor.js
@@ -0,0 +1,36 @@
+import React, {
+  Component
+} from 'react';
+import {
+  render
+} from 'react-dom';
+import Boostrap from 'bootstrap';
+import TinyMCE from 'react-tinymce';
+
+export default class TextEditor extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      config: {
+        plugins: 'link table code',
+        toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | code',
+        height: 200
+      }
+    }
+  }
+
+  refreshEditor(){
+    this.refs["editor"]._init(this.state.config, this.props.content);
+  }
+
+  render() {
+    return (
+      <div>
+      <TinyMCE ref="editor" content={this.props.content} config={this.state.config}  onChange={this.props.handleContentChange.bind(this)}/>
+      </div>
+    );
+  }
+}
+
+/* vim: set softtabstop=2:shiftwidth=2:expandtab */
