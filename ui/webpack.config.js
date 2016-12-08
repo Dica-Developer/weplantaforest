fb41c43301c25f266dffc365b58ac7261d493ce7
Gab0rB <gabor_barath@yahoo.de>
2016-12-08 17:21:32 +0100

    added green and red color for ok and remove icons

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -4,78 +4,79 @@ var inDev = process.env.NODE_ENV === 'development';
 var JS_FILE_NAME = inDev ? 'js/[name].bundle.js' : 'js/[name].[hash].js';
 
 module.exports = (function() {
-    var config = {
-        'modulesDirectories': ['node_modules']
-    };
+  var config = {
+    'modulesDirectories': ['node_modules']
+  };
 
-    // Define entry points
-    config.entry = {
-        'app': __dirname + '/src/js/app.js',
-        'vendor': [
-            'react',
-            'react-dom',
-            'react-router-component',
-            'bootstrap',
-            'axios',
-            'accounting'
-        ]
-    };
+  // Define entry points
+  config.entry = {
+    'app': __dirname + '/src/js/app.js',
+    'vendor': [
+      'react',
+      'react-dom',
+      'react-router-component',
+      'bootstrap',
+      'axios',
+      'accounting'
+    ]
+  };
 
-    config.output = {
-        'path': __dirname + '/dist',
-        'publicPath': inDev ? 'http://localhost:8080/' : '/',
-        'filename': JS_FILE_NAME
-    };
+  config.output = {
+    'path': __dirname + '/dist',
+    'publicPath': inDev ? 'http://localhost:8080/' : '/',
+    'filename': JS_FILE_NAME
+  };
 
-    config.module = {
-        loaders: [
-            {
-                test: /\.js?$/,
-                exclude: /node_modules/,
-                loader: 'babel',
-                query: {
-                    presets: ['react', 'es2015']
-                }
-            },
-            {
-              test: /\.less$/,
-              loader: "style!css!less"
-            }
-        ]
-    };
+  config.module = {
+    loaders: [{
+      test: /\.js?$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015']
+      }
+    }, {
+      test: /\.less$/,
+      loader: "style!css!less"
+    }, {
+      test: /\.(png|jpg)$/,
+      include: /dist/,
+      loader: 'url-loader?limit=5000000'
+    }]
+  };
 
-    config.plugins = [
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': `"${process.env.NODE_ENV}"`
-        }),
-        new webpack.optimize.CommonsChunkPlugin('vendor', JS_FILE_NAME),
-        new HtmlWebpackPlugin({
-            'template': __dirname + '/src/index.html',
-            'inject': 'body',
-            'minify': false
-        })
-    ];
+  config.plugins = [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': `"${process.env.NODE_ENV}"`
+    }),
+    new webpack.optimize.CommonsChunkPlugin('vendor', JS_FILE_NAME),
+    new HtmlWebpackPlugin({
+      'template': __dirname + '/src/index.html',
+      'inject': 'body',
+      'minify': false
+    })
+  ];
 
-    if (inDev) {
-        config.devtool = 'cheap-module-eval-source-map';
-    } else {
-        config.devtool = 'source-map';
-        config.plugins.push(
-            new webpack.optimize.UglifyJsPlugin(),
-            new webpack.optimize.DedupePlugin()
-        );
+  if (inDev) {
+    config.devtool = 'cheap-module-eval-source-map';
+  } else {
+    config.devtool = 'source-map';
+    config.plugins.push(
+      new webpack.optimize.UglifyJsPlugin(),
+      new webpack.optimize.DedupePlugin()
+    );
+  }
+
+  config.devServer = {
+    contentBase: __dirname + '/dist',
+    port: 8080,
+    stats: {
+      modules: false,
+      cached: false,
+      colors: true,
+      chunk: false
     }
+  };
 
-    config.devServer = {
-        contentBase: __dirname + '/dist',
-        port: 8080,
-        stats: {
-            modules: false,
-            cached: false,
-            colors: true,
-            chunk: false
-        }
-    };
-
-    return config;
+  return config;
 })();
