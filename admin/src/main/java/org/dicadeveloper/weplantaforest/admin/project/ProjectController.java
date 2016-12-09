d5e28e190f840ebe8a5b98c70478ad66088d537b
Gab0rB <gabor_barath@yahoo.de>
2016-12-09 17:26:01 +0100

    several controller changes to handle the project editor requests

--- admin/src/main/java/org/dicadeveloper/weplantaforest/admin/project/ProjectController.java
+++ admin/src/main/java/org/dicadeveloper/weplantaforest/admin/project/ProjectController.java
@@ -7,7 +7,9 @@ import javax.transaction.Transactional;
 
 import org.dicadeveloper.weplantaforest.admin.FileSystemInjector;
 import org.dicadeveloper.weplantaforest.admin.support.Uris;
+import org.dicadeveloper.weplantaforest.admin.tree.TreeRepository;
 import org.dicadeveloper.weplantaforest.common.image.ImageHelper;
+import org.dicadeveloper.weplantaforest.views.Views;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +19,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.fasterxml.jackson.annotation.JsonView;
 
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
@@ -31,7 +35,76 @@ public class ProjectController {
 
     private @NonNull ProjectImageRepository _projectImageRepository;
 
+    private @NonNull PriceRepository _priceRepository;
+    
+    private @NonNull TreeRepository _treeRepository;
+
     private @NonNull ImageHelper _imageHelper;
+
+    @RequestMapping(value = Uris.PROJECTS, method = RequestMethod.GET)
+    @JsonView(Views.ProjectNameAndId.class)
+    public ResponseEntity<?> getAllProjects() {
+        try {
+            Iterable<Project> projects = _projectRepository.findAll();
+            return new ResponseEntity<>(projects, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @RequestMapping(value = Uris.PROJECT, method = RequestMethod.GET)
+    @JsonView(Views.ProjectData.class)
+    public ResponseEntity<?> getProject(@RequestParam long projectId) {
+        try {
+            Project project = _projectRepository.findOne(projectId);
+            return new ResponseEntity<>(project, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @RequestMapping(value = Uris.ARTICLES, method = RequestMethod.GET)
+    @JsonView(Views.ProjectArticle.class)
+    public ResponseEntity<?> getArticlesForProject(@RequestParam long projectId) {
+        try {
+            List<ProjectArticle> articles = _projectArticleRepository.findByProjectId(projectId);
+            return new ResponseEntity<>(articles, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @RequestMapping(value = Uris.IMAGES, method = RequestMethod.GET)
+    @JsonView(Views.ProjectImage.class)
+    public ResponseEntity<?> getImagesForProject(@RequestParam long projectId) {
+        try {
+            List<ProjectImage> articles = _projectImageRepository.findProjectImagesToProjectByProjectId(projectId);
+            return new ResponseEntity<>(articles, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @RequestMapping(value = Uris.PROJECT_EDIT, method = RequestMethod.POST)
+    public ResponseEntity<?> editProject(@RequestBody Project project) {
+        try {
+            _projectRepository.save(project);
+            if (project.getArticles() != null) {
+                for (ProjectArticle article : project.getArticles()) {
+                    Price updatedPrice = article.getPrice();
+                    if (updatedPrice != null) {
+                        _priceRepository.save(updatedPrice);
+                    }
+
+                    article.setProject(project);
+                    _projectArticleRepository.save(article);
+                }
+            }
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
     @RequestMapping(value = Uris.PROJECT_CREATE, method = RequestMethod.POST)
     public ResponseEntity<?> createProject(@RequestBody Project project) {
@@ -62,29 +135,14 @@ public class ProjectController {
         }
     }
 
-    @RequestMapping(value = Uris.PROJECT_ADD_ARTICLE, method = RequestMethod.POST)
-    public ResponseEntity<?> addProjectArticle(@RequestBody ProjectArticle projectArticle, @RequestParam Long projectId) {
-        try {
-            if (_projectRepository.exists(projectId)) {
-                Project project = _projectRepository.findOne(projectId);
-                projectArticle.setProject(project);
-                _projectArticleRepository.save(projectArticle);
-                return new ResponseEntity<>(HttpStatus.OK);
-            } else {
-                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-            }
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
     @RequestMapping(value = Uris.PROJECT_REMOVE_ARTICLE, method = RequestMethod.POST)
     public ResponseEntity<?> removeArticle(@RequestParam Long articleId) {
-        if (_projectArticleRepository.exists(articleId)) {
+        if (_treeRepository.countAlreadyPlantedTreesByProjectArticleId(articleId) > 0) {
+            return new ResponseEntity<>("Du kannst diesen Projektartikel nicht löschen, da davon schon Bäume gepflanzt wurden.",HttpStatus.BAD_REQUEST);
+        } else {
             _projectArticleRepository.delete(articleId);
             return new ResponseEntity<>(HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
         }
     }
 
