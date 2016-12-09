ea20d4ba5a377146adca6dd61f39c45ad1fd8a98
Gab0rB <gabor_barath@yahoo.de>
2016-12-09 17:23:41 +0100

    added query to find articles by projectId

--- admin/src/main/java/org/dicadeveloper/weplantaforest/admin/project/ProjectArticleRepository.java
+++ admin/src/main/java/org/dicadeveloper/weplantaforest/admin/project/ProjectArticleRepository.java
@@ -9,9 +9,15 @@ import org.springframework.data.repository.query.Param;
 
 public interface ProjectArticleRepository extends CrudRepository<ProjectArticle, Long> {
 
+    public final static String FIND_ARTICLES_TO_PROJECT_BY_PROJECT_ID = "SELECT article FROM ProjectArticle article WHERE article.project.id = :projectId";
+    
     @Query
     public List<ProjectArticle> findByProject(@Param("project") Project project);
 
+    
+    @Query(value = FIND_ARTICLES_TO_PROJECT_BY_PROJECT_ID)
+    public List<ProjectArticle> findByProjectId(@Param("projectId") long projectId);
+    
     @Query
     public ProjectArticle findByProjectAndTreeType(@Param("project") Project project,
             @Param("treeType") TreeType treeType);
