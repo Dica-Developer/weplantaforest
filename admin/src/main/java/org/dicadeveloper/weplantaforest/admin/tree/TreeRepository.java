f6938439fa751f54a8f9b063b8c017127acc3fdc
Gab0rB <gabor_barath@yahoo.de>
2016-12-09 17:22:24 +0100

    changed counting query parameter from article object to articleId

--- admin/src/main/java/org/dicadeveloper/weplantaforest/admin/tree/TreeRepository.java
+++ admin/src/main/java/org/dicadeveloper/weplantaforest/admin/tree/TreeRepository.java
@@ -2,7 +2,6 @@ package org.dicadeveloper.weplantaforest.admin.tree;
 
 import java.util.List;
 
-import org.dicadeveloper.weplantaforest.admin.project.ProjectArticle;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
@@ -17,7 +16,7 @@ public interface TreeRepository extends PagingAndSortingRepository<Tree, Long> {
 
     public final static String FIND_TREETYPE_ID_BY_TREE_ID_QUERY = "SELECT tree.treeType.id  FROM Tree tree WHERE tree.id = :id";
 
-    public final static String COUNT_ALREADY_PLANTED_TREES_BY_PROJECTARTICLE = "SELECT COALESCE(sum(tree.amount), 0)  FROM Tree tree WHERE tree.projectArticle = :projectArticle";
+    public final static String COUNT_ALREADY_PLANTED_TREES_BY_PROJECTARTICLE = "SELECT COALESCE(sum(tree.amount), 0)  FROM Tree tree WHERE tree.projectArticle.articleId = :articleId";
     
     public final static String FIND_TREES_BY_USER_ID_QUERY = "SELECT tree  FROM Tree tree WHERE tree.owner.id = :ownerId";
     
@@ -28,7 +27,7 @@ public interface TreeRepository extends PagingAndSortingRepository<Tree, Long> {
     public Long findTreeTypeIdByTreeId(@Param("id") Long treeId);
 
     @Query(COUNT_ALREADY_PLANTED_TREES_BY_PROJECTARTICLE)
-    public Long countAlreadyPlantedTreesByProjectArticle(@Param("projectArticle") ProjectArticle projectArticle);
+    public Long countAlreadyPlantedTreesByProjectArticleId(@Param("articleId") long articleId);
     
     @Query(value = FIND_TREES_BY_USER_ID_QUERY)
     public Page<Tree> findTreesByUserId(@Param("ownerId") Long ownerId, Pageable page);
