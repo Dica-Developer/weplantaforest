ec5697669099a6b2aaa9f83926e41c828f6d9c02
Gab0rB <gabor_barath@yahoo.de>
2017-02-05 16:35:12 +0100

    created MainImageSlider entity, repository and controller with rquired methods in user module

---
+++ user/src/main/java/org/dicadeveloper/weplantaforest/mainSliderImage/MainSliderImageRepository.java
@@ -0,0 +1,9 @@
+package org.dicadeveloper.weplantaforest.mainSliderImage;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MainSliderImageRepository  extends PagingAndSortingRepository<MainSliderImage, Long>{
+
+}
