bbf59cbbb83f145f522086e314e54fa8f5c0e1e5
Gab0rB <gabor_barath@yahoo.de>
2017-02-13 23:42:30 +0100

    removed caching, no need atm here

--- admin/src/main/java/org/dicadeveloper/weplantaforest/admin/WeplantaforestAdminApplication.java
+++ admin/src/main/java/org/dicadeveloper/weplantaforest/admin/WeplantaforestAdminApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.boot.orm.jpa.EntityScan;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.hateoas.config.EnableEntityLinks;
@@ -13,7 +12,6 @@ import org.springframework.hateoas.config.EnableEntityLinks;
 @EnableEntityLinks
 @EntityScan(basePackageClasses = { WeplantaforestAdminApplication.class, Jsr310JpaConverters.class })
 @ComponentScan(basePackages = {"org.dicadeveloper.weplantaforest.*"})
-@EnableCaching
 public class WeplantaforestAdminApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
