205ec2efd081e3ccd1b9f62af9810d2f3685cfd5
Martin Schaaf <mschaaf@datameer.com>
2017-02-24 17:50:59 +0100

    * fix spring API changes in test code for article manager

--- article-manager/src/test/java/org/dicadeveloper/weplantaforest/articlemanager/article/ArticleControllerTest.java
+++ article-manager/src/test/java/org/dicadeveloper/weplantaforest/articlemanager/article/ArticleControllerTest.java
@@ -1,6 +1,7 @@
 package org.dicadeveloper.weplantaforest.articlemanager.article;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
@@ -14,7 +15,6 @@ import java.util.List;
 import javax.transaction.Transactional;
 
 import org.dicadeveloper.weplantaforest.articlemanager.FileSystemInjector;
-import org.dicadeveloper.weplantaforest.articlemanager.WeplantaforestArticleManagerApplication;
 import org.dicadeveloper.weplantaforest.articlemanager.articles.Article;
 import org.dicadeveloper.weplantaforest.articlemanager.articles.Article.ArticleType;
 import org.dicadeveloper.weplantaforest.articlemanager.articles.ArticleRepository;
@@ -31,22 +31,20 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.IntegrationTest;
-import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
-@SpringApplicationConfiguration(classes = WeplantaforestArticleManagerApplication.class)
-@IntegrationTest({ "spring.profiles.active=test" })
+@SpringBootTest({ "spring.profiles.active=test" })
 @DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
 public class ArticleControllerTest {
 
--- article-manager/src/test/java/org/dicadeveloper/weplantaforest/articlemanager/inject/DatabasePopulatorTest.java
+++ article-manager/src/test/java/org/dicadeveloper/weplantaforest/articlemanager/inject/DatabasePopulatorTest.java
@@ -5,7 +5,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.io.File;
 
 import org.dicadeveloper.weplantaforest.articlemanager.FileSystemInjector;
-import org.dicadeveloper.weplantaforest.articlemanager.WeplantaforestArticleManagerApplication;
 import org.dicadeveloper.weplantaforest.articlemanager.articles.ArticleRepository;
 import org.dicadeveloper.weplantaforest.articlemanager.articles.ParagraphRepository;
 import org.dicadeveloper.weplantaforest.articlemanager.dev.inject.DatabasePopulatorForArticleManager;
@@ -15,15 +14,13 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.IntegrationTest;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = WeplantaforestArticleManagerApplication.class)
-@IntegrationTest({ "spring.profiles.active=test" })
+@SpringBootTest({ "spring.profiles.active=test" })
 @DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
 public class DatabasePopulatorTest {
 
