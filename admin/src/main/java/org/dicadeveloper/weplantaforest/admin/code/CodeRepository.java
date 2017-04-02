e7a5e532fcd179c27a5c22ece9d066dd77446540
Gab0rB <gabor_barath@yahoo.de>
2017-04-02 22:55:44 +0200

    added query to find codes by event

--- admin/src/main/java/org/dicadeveloper/weplantaforest/admin/code/CodeRepository.java
+++ admin/src/main/java/org/dicadeveloper/weplantaforest/admin/code/CodeRepository.java
@@ -1,5 +1,8 @@
 package org.dicadeveloper.weplantaforest.admin.code;
 
+import java.util.List;
+
+import org.dicadeveloper.weplantaforest.admin.event.Event;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -8,5 +11,7 @@ public interface CodeRepository extends CrudRepository<Code, Long>{
 
     @Query
     public Code findByCode(@Param("code") String code);
+    
+    public List<Code> findByEvent(@Param(value = "event") Event event);
     
 }
