71bc32add45ffa6fe52a9032bf44d1b0156d3c1a
Gab0rB <gabor_barath@yahoo.de>
2017-04-13 14:24:50 +0200

    applied PreConditions in EventService

--- admin/src/main/java/org/dicadeveloper/weplantaforest/admin/event/EventService.java
+++ admin/src/main/java/org/dicadeveloper/weplantaforest/admin/event/EventService.java
@@ -12,6 +12,7 @@ import org.dicadeveloper.weplantaforest.admin.code.CodeRepository;
 import org.dicadeveloper.weplantaforest.admin.code.CodeService;
 import org.dicadeveloper.weplantaforest.admin.errorhandling.ErrorCodes;
 import org.dicadeveloper.weplantaforest.admin.errorhandling.IpatException;
+import org.dicadeveloper.weplantaforest.admin.errorhandling.IpatPreconditions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -31,80 +32,56 @@ public class EventService {
     CartRepository _cartRepository;
 
     public Event create(Event event) throws IpatException {
-        if (event != null) {
-            if (event.getId() == null) {
-                return _eventRepository.save(event);
-            } else {
-                throw new IpatException(ErrorCodes.EVENT_ALREADY_EXISTS);
-            }
-        } else {
-            throw new IpatException(ErrorCodes.EVENT_IS_NULL);
-        }
+        IpatPreconditions.checkNotNull(event, ErrorCodes.EVENT_IS_NULL);
+        IpatPreconditions.checkArgument(event.getId() == null, ErrorCodes.EVENT_ALREADY_EXISTS);
+        return _eventRepository.save(event);
     }
 
     @Transactional
     public void delete(Long eventId) throws IpatException {
         Event event = _eventRepository.findOne(eventId);
-        if (event != null) {
-            List<Cart> cartsForEvent = _cartRepository.findByEvent(event);
-            List<Code> codesForEvent = _codeRepository.findByEvent(event);
-            for(Cart cart : cartsForEvent){
-                if(cart.getCartState().equals(CartState.VERIFIED) ){
-                    throw new IpatException(ErrorCodes.CART_ALREADY_REDEEMED);
-                }
+        IpatPreconditions.checkNotNull(event, ErrorCodes.EVENT_IS_NULL);
+        List<Cart> cartsForEvent = _cartRepository.findByEvent(event);
+        List<Code> codesForEvent = _codeRepository.findByEvent(event);
+        for (Cart cart : cartsForEvent) {
+            if (cart.getCartState().equals(CartState.VERIFIED)) {
+                throw new IpatException(ErrorCodes.CART_ALREADY_REDEEMED);
             }
-            for(Cart cart : cartsForEvent){
-                _cartRepository.delete(cart);
-            }
-            
-            for(Code code : codesForEvent){
-                _codeRepository.delete(code);
-            }            
-            _eventRepository.delete(event);
-        } else {
-            throw new IpatException(ErrorCodes.EVENT_NOT_FOUND, eventId);
         }
+        for (Cart cart : cartsForEvent) {
+            _cartRepository.delete(cart);
+        }
+
+        for (Code code : codesForEvent) {
+            _codeRepository.delete(code);
+        }
+        _eventRepository.delete(event);
     }
 
     public Event update(Event event) throws IpatException {
-        if (event != null) {
-            Long eventId = event.getId();
-            if (eventId != null) {
-                event = _eventRepository.save(event);
-                return event;
-            } else {
-                throw new IpatException(ErrorCodes.EVENT_ID_IS_NULL);
-            }
-        } else {
-            throw new IpatException(ErrorCodes.EVENT_IS_NULL);
-        }
+        IpatPreconditions.checkNotNull(event, ErrorCodes.EVENT_IS_NULL);
+        IpatPreconditions.checkNotNull(event.getId(), ErrorCodes.EVENT_ALREADY_EXISTS);
+        return _eventRepository.save(event);
     }
 
     @Transactional
     public void generateCodes(Long eventId, List<Long> cartIds) throws IpatException {
         Event event = _eventRepository.findOne(eventId);
-        if (event != null) {
-            for (int i = 0; i < cartIds.size(); i++) {
-                generateCode(event, cartIds.get(i));
-            }
-        } else {
-            throw new IpatException(ErrorCodes.EVENT_NOT_FOUND, eventId);
+        IpatPreconditions.checkNotNull(event, ErrorCodes.EVENT_NOT_FOUND);
+        for (int i = 0; i < cartIds.size(); i++) {
+            generateCode(event, cartIds.get(i));
         }
     }
 
     private void generateCode(Event event, Long cartId) throws IpatException {
         Cart cart = _cartRepository.findOne(cartId);
-        if(cart != null){
-            Code code = _codeService.generateCode();
-            code.setEvent(event);
-            _codeRepository.save(code);
-            cart.setEvent(event);
-            cart.setCode(code);
-            _cartRepository.save(cart);            
-        }else{
-            throw new IpatException(ErrorCodes.CART_IS_NULL);
-        }
-
+        IpatPreconditions.checkNotNull(event, ErrorCodes.CART_IS_NULL);
+        Code code = _codeService.generateCode();
+        code.setEvent(event);
+        _codeRepository.save(code);
+        cart.setEvent(event);
+        cart.setCode(code);
+        _cartRepository.save(cart);
     }
 
 }
