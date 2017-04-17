25edbd1a627ac8aeaedbf2341d27810aaccb8f9a
Gab0rB <gabor_barath@yahoo.de>
2017-04-17 17:09:38 +0200

    added Cart to code and applied JsonView annotations

--- admin/src/main/java/org/dicadeveloper/weplantaforest/admin/code/Code.java
+++ admin/src/main/java/org/dicadeveloper/weplantaforest/admin/code/Code.java
@@ -9,8 +9,12 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 
+import org.dicadeveloper.weplantaforest.admin.cart.Cart;
 import org.dicadeveloper.weplantaforest.admin.event.Event;
 import org.dicadeveloper.weplantaforest.admin.gift.Gift;
+import org.dicadeveloper.weplantaforest.admin.views.Views;
+
+import com.fasterxml.jackson.annotation.JsonView;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -21,10 +25,12 @@ import lombok.Setter;
 public class Code {
 
     @Id
+    @JsonView(Views.CodeOverview.class)
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name ="_id")
     private Long id;
 
+    @JsonView(Views.CodeOverview.class)
     @Column(name ="_code",unique = true, nullable = false)
     private String code;
 
@@ -35,5 +41,10 @@ public class Code {
     @OneToOne(optional = true)
     @JoinColumn(name ="_gift__id")
     private Gift gift;
+    
+    @OneToOne(optional = true)
+    @JoinColumn(name ="_cart__cartId")
+    @JsonView(Views.CodeOverview.class)
+    private Cart cart;
 
 }
