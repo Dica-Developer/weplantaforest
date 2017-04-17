580cc94f4dd0bbbb5cf08f19aa13cb0fdcfd9476
Gab0rB <gabor_barath@yahoo.de>
2017-04-17 17:12:05 +0200

    changed naming of controller methods
    and added request to get event details

--- admin/src/main/java/org/dicadeveloper/weplantaforest/admin/event/EventController.java
+++ admin/src/main/java/org/dicadeveloper/weplantaforest/admin/event/EventController.java
@@ -2,15 +2,16 @@ package org.dicadeveloper.weplantaforest.admin.event;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.dicadeveloper.weplantaforest.admin.code.Code;
+import org.dicadeveloper.weplantaforest.admin.code.CodeRepository;
 import org.dicadeveloper.weplantaforest.admin.errorhandling.IpatException;
-import org.dicadeveloper.weplantaforest.views.Views;
+import org.dicadeveloper.weplantaforest.admin.views.Views;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,31 +21,51 @@ import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor(onConstructor = @__(@Autowired) )
-@RestController(value = EventController.REQUEST_URL)
+@RestController
 public class EventController {
 
     protected final Log LOG = LogFactory.getLog(EventController.class.getName());
 
-    public final static String REQUEST_URL = "/events";
+    public final static String REQUEST_URL = "/event";
 
     private @NonNull EventService eventService;
 
     private @NonNull EventRepository eventRepository;
 
-    @PostMapping
-    public ResponseEntity<?> createEvent(@RequestBody Event event) throws IpatException {
-        Event createdEvent = eventService.create(event);
-        return new ResponseEntity<>(createdEvent, HttpStatus.OK);
+    private @NonNull CodeRepository _codeRepository;
+
+    @PostMapping(value = REQUEST_URL)
+    @JsonView(Views.EventDetails.class)
+    public Event createEvent(@RequestBody Event event) throws IpatException {
+        return eventService.create(event);
+    }
+    
+    @PutMapping(value = REQUEST_URL)
+    @JsonView(Views.EventDetails.class)
+    public Event updateEvent(@RequestBody Event event) throws IpatException {
+        return eventService.update(event);
     }
 
-    @GetMapping
-    @JsonView(Views.Event.class)
-    public Iterable<?> getEvents() {
+    @GetMapping(value = REQUEST_URL + "s")
+    @JsonView(Views.EventOverview.class)
+    public Iterable<Event> getEvents() {
         return eventRepository.findAll();
     }
 
-    @DeleteMapping(value = "/{eventId}")
+    @GetMapping(value = REQUEST_URL + "/{eventId}")
+    @JsonView(Views.EventDetails.class)
+    public Event getEvent(@PathVariable Long eventId) throws IpatException {
+        return eventService.getEvent(eventId);
+    }
+
+    @DeleteMapping(value = REQUEST_URL + "/{eventId}")
     public void deleteEvent(@PathVariable Long eventId) throws IpatException {
         eventService.delete(eventId);
+    }
+
+    @GetMapping(value = REQUEST_URL + "/codes/{eventId}")
+    @JsonView(Views.CodeOverview.class)
+    public Iterable<Code> getCodesForEvent(@PathVariable Long eventId) {
+        return _codeRepository.findByEventId(eventId);
     }
 }
