ca6f6434025be9a9adf724e425007acccc0f5055
Gab0rB <gabor_barath@yahoo.de>
2017-04-29 21:32:00 +0200

    added disabled attribute to InputText

--- ui/src/js/common/components/InputText.js
+++ ui/src/js/common/components/InputText.js
@@ -19,7 +19,7 @@ export default class InputText extends Component {
   render() {
     return (
       <div>
-      <input type={this.props.type ? this.props.type : "text"} onBlur={this.updateValue.bind(this)}/>
+      <input type={this.props.type ? this.props.type : "text"} onBlur={this.updateValue.bind(this)} disabled={this.props.disabled}/>
       </div>
     );
   }
