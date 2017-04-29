41f1e0a554da0a1615e17e6993fccb2c536ddf3d
Gab0rB <gabor_barath@yahoo.de>
2017-04-29 21:32:54 +0200

    added method to handle backend errors from rest requests

--- ui/src/js/common/components/Notification.js
+++ ui/src/js/common/components/Notification.js
@@ -1,46 +1,45 @@
-import React, {
-  Component
-} from 'react';
-import {
-  render
-} from 'react-dom';
+import React, {Component} from 'react';
+import {render} from 'react-dom';
 import NotificationSystem from 'react-notification-system';
 import Boostrap from 'bootstrap';
 import MultiLineErrorObject from './MultiLineErrorObject';
 
 export default class Notification extends Component {
 
-  addNotification(title, message, type){
-    this.refs.notificationSystem.addNotification({
-      title: title,
-      position: 'tc',
-      autoDismiss: 0,
-      message:  message,
-      level: type
-    });
+  addNotification(title, message, type) {
+    this.refs.notificationSystem.addNotification({title: title, position: 'tc', autoDismiss: 0, message: message, level: type});
   }
 
-  addMultilineNotification(title, multilines, type){
+  addMultilineNotification(title, multilines, type) {
     this.refs.notificationSystem.addNotification({
       title: title,
       position: 'tc',
       autoDismiss: 0,
-      message:  '',
+      message: '',
       level: type,
-      children: (
-        <MultiLineErrorObject lines={multilines}/>
-      )
+      children: (<MultiLineErrorObject lines={multilines}/>)
     });
   }
 
-  addNotificationAtDifferentPos(title, message, type, position){
-    this.refs.notificationSystem.addNotification({
-      title: title,
-      position: position,
-      autoDismiss: 0,
-      message: message,
-      level: type
-    });
+  addNotificationAtDifferentPos(title, message, type, position) {
+    this.refs.notificationSystem.addNotification({title: title, position: position, autoDismiss: 0, message: message, level: type});
+  }
+
+  handleError(error) {
+    if (error.data.errorInfos) {
+      this.addMultilineNotification('Ein Fehler ist aufgetreten!', error.data.errorInfos, 'error');
+    } else {
+      //TODO: implement some kind of error report possibility
+      this.refs.notificationSystem.addNotification({title: 'Ein unerwarter Fehler ist aufgetreten!', position: 'tc', autoDismiss: 0, message: '', level: 'error'});
+      if (response instanceof Error) {
+        console.error('Error', error.message);
+      } else {
+        console.error(error.data);
+        console.error(error.status);
+        console.error(error.headers);
+        console.error(error.config);
+      }
+    }
   }
 
   render() {
