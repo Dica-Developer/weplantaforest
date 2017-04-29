c48d6827ae7542e8f58374d9c56318d5c57010a9
Gab0rB <gabor_barath@yahoo.de>
2017-04-29 21:28:33 +0200

    quick fix to avoid constraint violations when deleting carts
    * added todo to check this deeper

--- user/src/main/java/org/dicadeveloper/weplantaforest/scheduling/ScheduledTasks.java
+++ user/src/main/java/org/dicadeveloper/weplantaforest/scheduling/ScheduledTasks.java
@@ -6,6 +6,8 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.dicadeveloper.weplantaforest.cart.Cart;
 import org.dicadeveloper.weplantaforest.cart.CartRepository;
+import org.dicadeveloper.weplantaforest.code.Code;
+import org.dicadeveloper.weplantaforest.code.CodeRepository;
 import org.dicadeveloper.weplantaforest.trees.TreeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -25,6 +27,9 @@ public class ScheduledTasks {
 
     @Autowired
     private @NonNull TreeRepository _treeRepository;
+
+    @Autowired
+    private @NonNull CodeRepository _codeRepostiory;
     //
     // @Scheduled(fixedRate = DAY_IN_MILLISECONDS)
     // public void checkAbos() {
@@ -34,7 +39,19 @@ public class ScheduledTasks {
     @Scheduled(fixedRate = FOUR_HOURS_IN_MILLISECONDS)
     private void cleanUpInitialCarts() {
         List<Cart> carts = _cartRepository.findInitialCartsOlderThanFourHours(System.currentTimeMillis());
+        for (Cart cart : carts) {
+            // quick fix because of fk constraint violations
+            // TODO: if there's a code, there has to be also a Gift i think,
+            // think this should also be checked
+            if (cart.getCode() != null) {
+                Code code = cart.getCode();
+                code.setCart(null);
+                cart.setCode(null);
+                _codeRepostiory.delete(code);
+            }
+        }
         _cartRepository.delete(carts);
         LOG.info("deleted initial carts(" + carts.size() + ") and their trees");
+
     }
 }
