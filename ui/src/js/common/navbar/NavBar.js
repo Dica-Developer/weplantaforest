5d0ba7fb79d3a5eb2dd5ed1a7623f6ac78800679
Simon Aumayer <simonaumayer@Simons-MBP.fritz.box>
2017-10-05 18:12:46 +0200

    moved voucher menu items to admin menu

--- ui/src/js/common/navbar/NavBar.js
+++ ui/src/js/common/navbar/NavBar.js
@@ -100,8 +100,9 @@ export default class NavBar extends Component {
           <LoginMenuItem hash="login" updateNavbar={this.updateComponents.bind(this)} updateLanguage={this.updateLanguage.bind(this)}></LoginMenuItem>
           <MenuItem hash={"/user/" + localStorage.getItem('username')} inactive={this.state.profileLinksInActive}>Mein Profil</MenuItem>
           <MenuItem hash={"/tools/" + localStorage.getItem('username')} inactive={this.state.profileLinksInActive}>Tools</MenuItem>
-          <MenuItem hash={"/gifts/" + localStorage.getItem('username')} inactive={this.state.profileLinksInActive}>Gutscheine</MenuItem>
-          <MenuItem hash="6" inactive={this.state.profileLinksInActive}>Abonnements</MenuItem>
+          <MenuItem hash={"/gifts/" + localStorage.getItem('username')} inactive={this.state.profileLinksInActive}>Gutscheinübersicht</MenuItem>
+          <MenuItem hash={"/plantGift/5"} inactive={this.state.profileLinksInActive}>Gutschein erstellen</MenuItem>
+          <MenuItem hash={"/gift/redeem"} inactive={this.state.profileLinksInActive}>Gutschein einlösen</MenuItem>
           <MenuItem hash={"/receipts/" + localStorage.getItem('username')} inactive={this.state.profileLinksInActive}>Spendenquittungen</MenuItem>
           <BackOfficeMenuItem hash="/backOffice">Backoffice</BackOfficeMenuItem>
         </Menu>
