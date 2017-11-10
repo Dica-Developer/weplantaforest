b709ca15550c4bef5e29d42f72357123a4abf64f
gabor <gbarath@quartoninternational.com>
2017-11-10 09:48:46 +0100

    fixed tests

--- article-manager/src/test/java/org/dicadeveloper/weplantaforest/articlemanager/inject/DatabasePopulatorTest.java
+++ article-manager/src/test/java/org/dicadeveloper/weplantaforest/articlemanager/inject/DatabasePopulatorTest.java
@@ -50,7 +50,7 @@ public class DatabasePopulatorTest {
     public void testInsertArticles() throws Exception {
         _databasePopulator.insertUsers();
         _databasePopulator.insertArticles();
-        assertThat(_articleRepository.count()).isEqualTo(115);
+        assertThat(_articleRepository.count()).isEqualTo(120);
     }
 
     @Test
@@ -58,7 +58,7 @@ public class DatabasePopulatorTest {
         _databasePopulator.insertUsers();
         _databasePopulator.insertArticles();
         _databasePopulator.insertParagraphsToArticles();
-        assertThat(_paragraphRepository.count()).isEqualTo(345);
+        assertThat(_paragraphRepository.count()).isEqualTo(360);
     }
     
     @Test
