03a29ee5e4fe1ce0f6990aaa6facd8b10c5981f5
gabor <gbarath@quartoninternational.com>
2017-11-10 11:11:13 +0100

    added validation to contact form

--- ui/src/js/common/footer/pages/contact.js
+++ ui/src/js/common/footer/pages/contact.js
@@ -7,6 +7,8 @@ import {
 import Boostrap from 'bootstrap';
 import axios from 'axios';
 
+import Notification from '../../components/Notification';
+
 require("./contact.less");
 
 export default class Contact extends Component {
@@ -14,7 +16,14 @@ export default class Contact extends Component {
   constructor() {
     super();
     this.state = {
-      contact: []
+      contact: [],
+      form: {
+        reason: '',
+        name: '',
+        mail: '',
+        phone: '',
+        message: ''
+      }
     }
   }
 
@@ -22,7 +31,9 @@ export default class Contact extends Component {
     var that = this;
     axios.get('http://localhost:8082/articles?articleType=CONTACT&language=' + localStorage.getItem('language')).then(function(response) {
       var result = response.data;
-      that.setState({contact: result});
+      that.setState({
+        contact: result
+      });
     }).catch(function(response) {
       if (response instanceof Error) {
         console.error('Error', response.message);
@@ -33,6 +44,26 @@ export default class Contact extends Component {
         console.error(response.config);
       }
     });
+  }
+
+  submit() {
+    if (this.validateForm()) {
+      //send message here
+    } else {
+      this.refs.notification.addNotification('Fehler!', 'Bitte füll alle notwendigen(mit einem * markierte) Felder aus!', 'error');
+    }
+  }
+
+  validateForm() {
+    if (this.state.form.name === '' || this.state.form.mail === '' || this.state.form.message === '') {
+      return false;
+    }
+    return true;
+  }
+
+  updateValue(toUpdate, event){
+    this.state.form[toUpdate] = event.target.value;
+    this.forceUpdate();
   }
 
   render() {
@@ -58,7 +89,7 @@ export default class Contact extends Component {
           <div className="row">
             <div className="form-group">
               <label htmlFor="contact_type">Ihr Anliegen:</label>
-                <select className="form-control" id="contact_type" defaultValue={0}>
+                <select className="form-control" id="contact_type" defaultValue={0} onChange={(e) => this.updateValue('reason', e)}>
                   <option value={0}>Frage</option>
                   <option value={1}>Presse & Media</option>
                   <option value={2}>Kooperation</option>
@@ -66,24 +97,25 @@ export default class Contact extends Component {
             </div>
             <div className="form-group">
               <label htmlFor="firstname_familyname">Vorname Nachname*:</label>
-              <input type="text" className="form-control" id="firstname_familyname" placeholder="Bitte geben Sie Ihren Vor- und Nachnamen ein." />
+              <input type="text" className="form-control" id="firstname_familyname" placeholder="Bitte geben Sie Ihren Vor- und Nachnamen ein." onBlur={(e) => this.updateValue('name', e)}/>
             </div>
             <div className="form-group">
               <label htmlFor="e_mail">E-Mailadresse*:</label>
-              <input type="email" className="form-control" id="e_mail" placeholder="Bitte geben Sie Ihre E-mailadresse ein." />
+              <input type="email" className="form-control" id="e_mail" placeholder="Bitte geben Sie Ihre E-mailadresse ein." onBlur={(e) => this.updateValue('mail', e)}/>
             </div>
             <div className="form-group">
               <label htmlFor="phone_number">Telefonnummer:</label>
-              <input type="text" className="form-control" id="phone_number" placeholder="Bitte geben Sie Ihre Telefonnummer ein." />
+              <input type="text" className="form-control" id="phone_number" placeholder="Bitte geben Sie Ihre Telefonnummer ein." onBlur={(e) => this.updateValue('phone', e)} />
             </div>
             <div className="form-group">
               <label htmlFor="personal_message">Ihre Nachricht*:</label>
-              <textarea className="form-control" rows="10" id="personal_message" placeholder="Bitte geben Sie Ihre Nachricht ein." />
+              <textarea className="form-control" rows="10" id="personal_message" placeholder="Bitte geben Sie Ihre Nachricht ein." onBlur={(e) => this.updateValue('message', e)}/>
             </div>
             <div className="form-group">
-              <button type="submit" className="btn btn-default">Absenden</button>
+              <button type="submit" className="btn btn-default" onClick={this.submit.bind(this)}>Absenden</button>
             </div>
           </div>
+          <Notification ref="notification"/>
         </div>
     );
   }
