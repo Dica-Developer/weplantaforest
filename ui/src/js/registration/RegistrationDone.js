aafcc37d78e40165d7d3a3594baf01af78ed19ae
Simon Aumayer <simon.aumayer@aboutlabs.de>
2018-04-26 15:14:02 +0200

    styles registration forms

--- ui/src/js/registration/RegistrationDone.js
+++ ui/src/js/registration/RegistrationDone.js
@@ -6,10 +6,12 @@ export default class RegistrationDone extends Component {
 
   render() {
     return (
-      <div className="col-md-12 registrationDone">
-        <h1>Du wurdest angemeldet!</h1>
-        <div>
-         Um deinen Nutzer zu aktivieren, haben wir dir einen Aktivierungslink an die angegebene E-Mail Adresse geschickt.
+      <div className="registrationDone">
+        <div className="row">
+          <div className="col-md-12 align-left">
+            <h1>Du wurdest angemeldet!</h1>
+            <p>Um deinen Nutzer zu aktivieren, haben wir dir einen Aktivierungslink an die angegebene E-Mail Adresse geschickt.</p>
+          </div>
         </div>
       </div>
     );
