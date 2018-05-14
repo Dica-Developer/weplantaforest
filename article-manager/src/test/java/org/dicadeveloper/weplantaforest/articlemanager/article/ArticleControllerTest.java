6e34e8150fa6a54e07566e5a3efb5d2f927b8dca
Gab0rB <gabor_barath@yahoo.de>
2018-05-14 21:48:12 +0200

    fixed test

--- article-manager/src/test/java/org/dicadeveloper/weplantaforest/articlemanager/article/ArticleControllerTest.java
+++ article-manager/src/test/java/org/dicadeveloper/weplantaforest/articlemanager/article/ArticleControllerTest.java
@@ -1,7 +1,7 @@
 package org.dicadeveloper.weplantaforest.articlemanager.article;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
@@ -31,13 +31,13 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -124,7 +124,7 @@ public class ArticleControllerTest {
         assertThat(_articleRepository.count()).isEqualTo(1L);
         assertThat(_paragraphRepository.count()).isEqualTo(1L);
 
-        mockMvc.perform(post("/backOffice/article/delete").contentType(MediaType.APPLICATION_JSON)
+        mockMvc.perform(delete("/backOffice/article").contentType(MediaType.APPLICATION_JSON)
                                                             .param("articleId", "1"))
                .andExpect(status().isOk());
 
