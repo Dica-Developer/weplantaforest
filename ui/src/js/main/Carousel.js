1309c970a457fdff77b6dadb7e1bdfb246b6859c
Gab0rB <gabor_barath@yahoo.de>
2018-06-12 17:37:58 +0100

    remove carousel element, when there are no images to display

--- ui/src/js/main/Carousel.js
+++ ui/src/js/main/Carousel.js
@@ -30,42 +30,50 @@ export default class Carousel extends Component {
   }
 
   render() {
-    return (
-      <div id="carousel-example-generic" className="carousel slide" data-ride="carousel">
-        <ol className="carousel-indicators">
-          {this.state.slides.map(function(slide, index) {
-            var activeItem = '';
-            if (0 === index) {
-              activeItem = 'active';
-            }
-            return (
-              <li data-target="#carousel-example-generic" data-slide-to={index} className={activeItem} key={index}></li>
-            );
-          })}
-        </ol>
-        <div className="carousel-inner" role="listbox">
-          {this.state.slides.map(function(slide, index) {
-            var activeItem = 'item';
-            if (0 === index) {
-              activeItem = 'item active';
-            }
-            let imageUrl = 'http://localhost:8081/mainSliderImage/' + slide.imageFileName + '/1140/570';
+    let content = '';
+    if(this.state.slides.length > 0){
+      content =   <div id="carousel-example-generic" className="carousel slide" data-ride="carousel">
+          <ol className="carousel-indicators">
+            {this.state.slides.map(function(slide, index) {
+              var activeItem = '';
+              if (0 === index) {
+                activeItem = 'active';
+              }
+              return (
+                <li data-target="#carousel-example-generic" data-slide-to={index} className={activeItem} key={index}></li>
+              );
+            })}
+          </ol>
+          <div className="carousel-inner" role="listbox">
+            {this.state.slides.map(function(slide, index) {
+              var activeItem = 'item';
+              if (0 === index) {
+                activeItem = 'item active';
+              }
+              let imageUrl = 'http://localhost:8081/mainSliderImage/' + slide.imageFileName + '/1140/570';
 
-            return (
-              <div className={activeItem} key={index}>
-                <img src={imageUrl} width="1140" height="570" />
-              </div>
-            );
-          })}
-        </div>
-        <a className="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-          <span className="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-          <span className="sr-only">Previous</span>
-        </a>
-        <a className="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-          <span className="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-          <span className="sr-only">Next</span>
-        </a>
+              return (
+                <div className={activeItem} key={index}>
+                  <img src={imageUrl} width="1140" height="570" />
+                </div>
+              );
+            })}
+          </div>
+          <a className="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+            <span className="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span className="sr-only">Previous</span>
+          </a>
+          <a className="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+            <span className="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span className="sr-only">Next</span>
+          </a>
+        </div>;
+    }else{
+      content = '';
+    }
+    return (
+      <div>
+        {content}
       </div>
     );
   }
