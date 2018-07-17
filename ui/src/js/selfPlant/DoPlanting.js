1d6acc420f5f50a2ee32aca1b0e1c73a5e6510c3
Gab0rB <gabor_barath@yahoo.de>
2018-07-17 19:08:00 +0200

    selfplantpage: moved first treetype(other) to the last position

--- ui/src/js/selfPlant/DoPlanting.js
+++ ui/src/js/selfPlant/DoPlanting.js
@@ -36,6 +36,8 @@ export default class DoPlanting extends Component {
     var that = this;
     axios.get('http://localhost:8081/treeTypes').then(function(response) {
       var result = response.data;
+      //move first element('Other') to the last position
+      result.splice(result.length - 1, 0, result.splice(0, 1)[0]);
       that.setState({treeTypes: result, treeType: that.state.treeTypes[0]});
     }).catch(function(response) {
       if (response instanceof Error) {
