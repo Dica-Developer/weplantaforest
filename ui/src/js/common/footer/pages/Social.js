4928c419fca8116b0d74a576d95c8aa63d6ef555
Gab0rB <gabor_barath@yahoo.de>
2018-09-26 17:07:33 +0200

    added social media logos and linked them

--- ui/src/js/common/footer/pages/Social.js
+++ ui/src/js/common/footer/pages/Social.js
@@ -22,13 +22,19 @@ export default class Social extends Component {
           </div>
           <div className="row">
             <div className="col-md-4">
-              FACEBOOK
+              <a href="https://de-de.facebook.com/iplantatree/">
+                <img src="/assets/images/facebook.png" alt={'facebook'} height="80"/>
+              </a>
             </div>
             <div className="col-md-4">
-              TWITTER
+              <a href="https://twitter.com/iplantatree?lang=de">
+                <img src="/assets/images/twitter.png" alt={'twitter'} height="80"/>
+              </a>
             </div>
             <div className="col-md-4">
-              YOUTUBE
+              <a href="https://www.youtube.com/channel/UC3N_Y8sxT7HA0fIXRLvSi7A">
+                <img src="/assets/images/youtube.jpg" alt={'youtube'} height="80"/>
+              </a>
             </div>
           </div>
         </div>
