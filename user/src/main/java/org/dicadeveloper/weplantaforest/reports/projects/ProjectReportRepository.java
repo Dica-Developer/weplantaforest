747371108c2fe452b1bc56bc37d8293bec0f8dc4
Gab0rB <gabor_barath@yahoo.de>
2018-10-04 15:20:32 +0200

    added visible flag to project queries

--- user/src/main/java/org/dicadeveloper/weplantaforest/reports/projects/ProjectReportRepository.java
+++ user/src/main/java/org/dicadeveloper/weplantaforest/reports/projects/ProjectReportRepository.java
@@ -18,37 +18,35 @@ public interface ProjectReportRepository extends CrudRepository<Project, Long> {
             + "FROM Project project JOIN project.articles articles WHERE articles.project = project GROUP BY project.name ORDER BY project.id DESC";
 
     public final static String FIND_ALL_PROJECTS_COUNT_QUERY = "SELECT count(distinct project.name) from Project project";
-    
+
     public final static String FIND_ALL_ACTIVE_PROJECTS_QUERY = "SELECT new org.dicadeveloper.weplantaforest.reports.projects.ProjectReportData( "
             + "project.id, project.name, project.imageFileName, project.description, project.longitude, project.latitude, SUM(articles.amount), "
             + "COALESCE((SELECT SUM(tree.amount) FROM Tree as tree WHERE tree.projectArticle IN(SELECT article FROM ProjectArticle article WHERE article.project = project)),0), project.shopActive) "
-            + "FROM Project project JOIN project.articles articles WHERE project.shopActive = true AND articles.project = project GROUP BY project.name ORDER BY project.id DESC";
-   
+            + "FROM Project project JOIN project.articles articles WHERE project.shopActive = true AND project.visible = true AND articles.project = project GROUP BY project.name ORDER BY project.id DESC";
+
     public final static String FIND_ALL_INACTIVE_PROJECTS_QUERY = "SELECT new org.dicadeveloper.weplantaforest.reports.projects.ProjectReportData( "
             + "project.id, project.name, project.imageFileName, project.description, project.longitude, project.latitude, SUM(articles.amount), "
             + "COALESCE((SELECT SUM(tree.amount) FROM Tree as tree WHERE tree.projectArticle IN(SELECT article FROM ProjectArticle article WHERE article.project = project)),0), project.shopActive) "
-            + "FROM Project project JOIN project.articles articles WHERE project.shopActive = false AND articles.project = project GROUP BY project.name ORDER BY project.id DESC";
-
+            + "FROM Project project JOIN project.articles articles WHERE project.shopActive = false AND project.visible = true AND articles.project = project GROUP BY project.name ORDER BY project.id DESC";
 
     public final static String FIND_POJECT_DATA_BY_NAME = "SELECT new org.dicadeveloper.weplantaforest.reports.projects.ProjectReportData( "
             + "project.id, project.name, project.imageFileName, project.description, project.longitude, project.latitude, SUM(articles.amount), "
             + "COALESCE((SELECT SUM(tree.amount) FROM Tree as tree WHERE tree.projectArticle IN(SELECT article FROM ProjectArticle article WHERE article.project = project)),0), project.shopActive) "
-            + "FROM Project project JOIN project.articles articles WHERE project.name = :projectName "
-            + "AND articles.project = project";
-    
+            + "FROM Project project JOIN project.articles articles WHERE project.name = :projectName " + "AND articles.project = project";
+
     @Query(value = FIND_ALL_PROJECTS_QUERY, countQuery = FIND_ALL_PROJECTS_COUNT_QUERY)
     Page<ProjectReportData> getAllProjects(Pageable page);
-    
-    @Query(value = FIND_ALL_ACTIVE_PROJECTS_QUERY, countQuery = FIND_ALL_PROJECTS_COUNT_QUERY + " WHERE project.shopActive = true")
+
+    @Query(value = FIND_ALL_ACTIVE_PROJECTS_QUERY, countQuery = FIND_ALL_PROJECTS_COUNT_QUERY + " WHERE project.shopActive = true AND project.visible = true")
     List<ProjectReportData> getActiveProjects();
-    
-    @Query(value = FIND_ALL_INACTIVE_PROJECTS_QUERY, countQuery = FIND_ALL_PROJECTS_COUNT_QUERY + " WHERE project.shopActive = false")
+
+    @Query(value = FIND_ALL_INACTIVE_PROJECTS_QUERY, countQuery = FIND_ALL_PROJECTS_COUNT_QUERY + " WHERE project.shopActive = false AND project.visible = true")
     Page<ProjectReportData> getInActiveProjects(Pageable page);
-    
+
     @Query(value = FIND_POJECT_DATA_BY_NAME)
     ProjectReportData getProjectDataByProjectName(@Param("projectName") String projectName);
-    
-    @Query(value = "SELECT p FROM Project p where p.shopActive=true ORDER BY p.id DESC")
+
+    @Query(value = "SELECT p FROM Project p where p.visible=true ORDER BY p.id DESC")
     public Page<Project> active(Pageable pageable);
 
 }
