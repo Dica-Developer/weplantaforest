f6bee38ad3a3c00a0083a760884045d145555ac5
Martin Schaaf <703355+mschaaf@users.noreply.github.com>
2019-11-05 21:59:09 +0100

    * upgrade dependencies

--- admin/src/main/java/org/dicadeveloper/weplantaforest/admin/cart/CartService.java
+++ admin/src/main/java/org/dicadeveloper/weplantaforest/admin/cart/CartService.java
@@ -6,7 +6,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
