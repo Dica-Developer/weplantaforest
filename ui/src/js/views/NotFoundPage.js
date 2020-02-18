d3d13444b20a7583018e1fc62f2cf68a46ba86a2
Martin Schaaf <703355+mschaaf@users.noreply.github.com>
2020-02-18 22:34:25 +0100

    redesign 404 page

--- ui/src/js/views/NotFoundPage.js
+++ ui/src/js/views/NotFoundPage.js
@@ -4,8 +4,9 @@ export default class NotFoundPage extends Component {
   render() {
     return (
       <div>
-        <div className="container">
-          <img width="100%" height="100%" src="https://upload.wikimedia.org/wikipedia/commons/a/ad/Tunguska.png"/>
+        <div className="container paddingTopBottom15">
+          <img width="100%" height="100%" src="/assets/images/404.jpg" style={{position:'relative'}}/>
+          <h1 style={{position:'absolute', bottom:'53%', backgroundColor: '#4F3A2C', color: 'white'}}>Die Seite existiert nicht.<br /><a style={{color: 'white'}} href="/">Fang doch nochmal von vorn an.</a></h1>
         </div>
       </div>
     );
