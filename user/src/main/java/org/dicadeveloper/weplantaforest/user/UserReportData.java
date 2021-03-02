da976276aa74c9bde1f6a3170bc3e1b52d3c2c00
Martin Schaaf <mschaaf@quartoninternational.com>
2021-03-02 22:25:44 +0100

    allow null as image file name

--- user/src/main/java/org/dicadeveloper/weplantaforest/user/UserReportData.java
+++ user/src/main/java/org/dicadeveloper/weplantaforest/user/UserReportData.java
@@ -5,13 +5,28 @@ import org.dicadeveloper.weplantaforest.reports.co2.Co2Data;
 
 import lombok.Getter;
 import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
-@RequiredArgsConstructor
 @Getter
 @Setter
 public class UserReportData {
+
+    public UserReportData(@NonNull String userName, String imageFileName, @NonNull String mail, @NonNull Long regDate, @NonNull Long lastVisit, @NonNull OrganizationType organizationType,
+            @NonNull String teamName, @NonNull String aboutMe, @NonNull String location, @NonNull String organisation, @NonNull String homepage, @NonNull Language lang, @NonNull Boolean newsletter) {
+        this.userName = userName;
+        this.imageFileName = imageFileName;
+        this.mail = mail;
+        this.regDate = regDate;
+        this.lastVisit = lastVisit;
+        this.organizationType = organizationType;
+        this.teamName = teamName;
+        this.aboutMe = aboutMe;
+        this.location = location;
+        this.organisation = organisation;
+        this.homepage = homepage;
+        this.lang = lang;
+        this.newsletter = newsletter;
+    }
 
     @NonNull
     String userName;
