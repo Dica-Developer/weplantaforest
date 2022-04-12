2e8c4f15945c4bd180253a9a34308c30372e66d9
Gabor Barath <gabor_barath@yahoo.de>
2022-04-12 19:01:23 +0200

    fixed #346

--- ui2022/src/app/back-office-page/carts/cart-grid/cart-grid.component.ts
+++ ui2022/src/app/back-office-page/carts/cart-grid/cart-grid.component.ts
@@ -10,7 +10,11 @@ import {
 import { GridHelper } from '../../../util/grid.helper';
 import { GridCheckboxComponent } from '../../../util/grid-components/grid-checkbox/grid-checkbox.component';
 import { GridSelectComponent } from '../../../util/grid-components/grid-select/grid-select.component';
-import { updateStatus, CartDetails, selectCartDetails } from '../../../store/carts.store';
+import {
+  updateStatus,
+  CartDetails,
+  selectCartDetails,
+} from '../../../store/carts.store';
 import { GridCartActionsComponent } from '../../../util/grid-components/grid-cart-actions/grid-cart-actions.component';
 
 @Component({
@@ -19,7 +23,6 @@ import { GridCartActionsComponent } from '../../../util/grid-components/grid-car
   styleUrls: ['./cart-grid.component.scss'],
 })
 export class CartGridComponent implements OnInit {
-
   allColumns: ColDef[] = [
     {
       field: 'username',
@@ -149,7 +152,7 @@ export class CartGridComponent implements OnInit {
           },
         };
       },
-    }
+    },
   ];
 
   subsetOfColumns: ColDef[] = [
@@ -172,6 +175,89 @@ export class CartGridComponent implements OnInit {
       valueFormatter: this.gridHelper.dateFormatter,
       sortable: true,
     },
+    {
+      field: 'firstName',
+      headerName: 'Vorname',
+      filter: 'agTextColumnFilter',
+      sortable: true,
+      comparator: this.gridHelper.caseInsensitiveComparator,
+      editable: true,
+      valueSetter: (params) => this.updateAddress(params),
+    },
+    {
+      field: 'lastName',
+      headerName: 'Nachname',
+      filter: 'agTextColumnFilter',
+      sortable: true,
+      comparator: this.gridHelper.caseInsensitiveComparator,
+      editable: true,
+      valueSetter: (params) => this.updateAddress(params),
+    },
+    {
+      field: 'company',
+      headerName: 'Unternehmen',
+      filter: 'agTextColumnFilter',
+      sortable: true,
+      comparator: this.gridHelper.caseInsensitiveComparator,
+      editable: true,
+      valueSetter: (params) => this.updateAddress(params),
+    },
+    {
+      field: 'paymentType',
+      headerName: 'Zahlungsart',
+      filter: 'agTextColumnFilter',
+      sortable: true,
+      comparator: this.gridHelper.caseInsensitiveComparator,
+    },
+    {
+      field: 'receiptable',
+      headerName: 'SQ',
+      cellRendererSelector: (params) => {
+        return {
+          component: 'checkboxRenderer',
+          params: {
+            value: params.data.receiptable,
+            disabled:
+              params.data.receiptId !== null &&
+              params.data.receiptId !== undefined,
+            id: params.data.id,
+            valueChange: (cartId, value) =>
+              this.store.dispatch(updateReceiptableFlag({ cartId, value })),
+          },
+        };
+      },
+    },
+    {
+      field: 'status',
+      headerName: 'Status',
+      filter: 'agTextColumnFilter',
+      sortable: true,
+      editable: (params) => {
+        if (params.data.status === 'DISCARDED') {
+          return false;
+        } else {
+          return true;
+        }
+      },
+      cellEditor: 'selectRenderer',
+      cellEditorParams: {
+        valueList: this.gridHelper.getCartStates(),
+        valueChange: (cartId, value) =>
+          this.store.dispatch(updateStatus({ cartId, value })),
+      },
+    },
+    {
+      field: 'id',
+      headerName: 'Actions',
+      cellRendererSelector: (params) => {
+        return {
+          component: 'cartActionRenderer',
+          params: {
+            value: params.data.id,
+          },
+        };
+      },
+    },
   ];
 
   colDefs: ColDef[] = this.subsetOfColumns;
@@ -181,7 +267,7 @@ export class CartGridComponent implements OnInit {
     components: {
       checkboxRenderer: GridCheckboxComponent,
       selectRenderer: GridSelectComponent,
-      cartActionRenderer: GridCartActionsComponent
+      cartActionRenderer: GridCartActionsComponent,
     },
   };
 
