39f533ad8badee464c08f434e44ee6c055009239
Gabor Barath <gabor_barath@yahoo.de>
2022-05-03 17:29:50 +0200

    added config for staging environment

---
+++ ui2022/src/environments/environment.staging.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  backendUrl: 'https://iplantatree.org/s/u',
+  backendAdminUrl: 'https://iplantatree.org/s/a',
+  backendArticleManagerUrl: 'https://iplantatree.org/s/m',
+
+};
