a4f78f01acf681fcf89a43be45ab6bf3934e3588
tolga <tolga.schneider@thequalitygate.com>
2022-12-08 18:37:10 +0100

    added forest tour component

---
+++ ui2022/src/app/pages/home-page/components/forest-tour/forest-tour.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ForestTourComponent } from './forest-tour.component';
+
+describe('ForestTourComponent', () => {
+  let component: ForestTourComponent;
+  let fixture: ComponentFixture<ForestTourComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ForestTourComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ForestTourComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
