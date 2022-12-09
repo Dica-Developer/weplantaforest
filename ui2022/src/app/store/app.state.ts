b95c1c350a2a2aea09ba2d8ea45f99c96a52415f
Gabor Barath <gabor_barath@yahoo.de>
2022-12-09 13:36:51 +0100

    added PagedData interface

--- ui2022/src/app/store/app.state.ts
+++ ui2022/src/app/store/app.state.ts
@@ -10,21 +10,31 @@ import { ContentState } from './content.store';
 import { EventsState } from './events.store';
 import { TeamState } from './team.store';
 import { PlantbagState } from './plantbag.store';
-import { TreeState } from "./tree.store";
+import { TreeState } from './tree.store';
+import { RankingState } from './ranking.store';
 
+export interface PagedData<T> {
+  totalPages: number;
+  totalElements: number;
+  numberOfElements: number;
+  last: boolean;
+  first: boolean;
+  content: T[];
+}
 
-export interface AppState{
-  readonly auth: AuthState,
-  readonly profile: ProfileState,
-  readonly carts: CartsState,
-  readonly user: UserState,
-  readonly projects: ProjectState,
-  readonly treeTypes: TreeTypeState,
-  readonly errors: ErrorState,
-  readonly successMessages: SuccessMessageState,
-  readonly content: ContentState,
-  readonly event: EventsState,
-  readonly teams: TeamState,
-  readonly plantbag: PlantbagState,
-  readonly trees: TreeState
+export interface AppState {
+  readonly auth: AuthState;
+  readonly profile: ProfileState;
+  readonly carts: CartsState;
+  readonly user: UserState;
+  readonly projects: ProjectState;
+  readonly treeTypes: TreeTypeState;
+  readonly errors: ErrorState;
+  readonly successMessages: SuccessMessageState;
+  readonly content: ContentState;
+  readonly event: EventsState;
+  readonly teams: TeamState;
+  readonly plantbag: PlantbagState;
+  readonly trees: TreeState;
+  readonly ranking: RankingState;
 }
