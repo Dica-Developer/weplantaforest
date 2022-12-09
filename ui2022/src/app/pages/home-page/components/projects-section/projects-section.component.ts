26dd524aee1a467ca01156d4d72f327f1eaf560c
tolga <tolga.schneider@thequalitygate.com>
2022-12-09 14:23:43 +0100

    implemented project tile and horizontal scrollview

--- ui2022/src/app/pages/home-page/components/projects-section/projects-section.component.ts
+++ ui2022/src/app/pages/home-page/components/projects-section/projects-section.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import { AppState } from 'src/app/store/app.state';
+import { loadProjectReports, selectProjectReports } from 'src/app/store/project-report.store';
 
 @Component({
   selector: 'app-projects-section',
   templateUrl: './projects-section.component.html',
-  styleUrls: ['./projects-section.component.scss']
+  styleUrls: ['./projects-section.component.scss'],
 })
 export class ProjectsSectionComponent implements OnInit {
+  selectProjectReportsSub: Subscription;
+  projectReports$ = this.store.select(selectProjectReports);
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private store: Store<AppState>) {
+    this.store.dispatch(loadProjectReports());
   }
 
+  ngOnInit(): void {}
 }
--- ui2022/src/app/util/common-components/project-tile/project-tile.component.ts
+++ ui2022/src/app/util/common-components/project-tile/project-tile.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-project-tile',
   templateUrl: './project-tile.component.html',
-  styleUrls: ['./project-tile.component.scss']
+  styleUrls: ['./project-tile.component.scss'],
 })
 export class ProjectTileComponent implements OnInit {
+  @Input() projectReport;
+  imgUrl: string;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    // this.imgUrl =
+    //   environment.backendUrl +
+    //   '/project/image/' +
+    //   this.projectReport.projectImageFileName
+    this.imgUrl = environment.baseUrl + '/assets/forest.png';
   }
-
 }
