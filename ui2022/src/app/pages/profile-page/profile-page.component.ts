a408e3a8395618dec805dd9c443235bc725861d5
tolga <tolga.schneider@thequalitygate.com>
2023-01-28 04:34:44 +0100

    added plantings in profile page

--- ui2022/src/app/pages/profile-page/profile-page.component.ts
+++ ui2022/src/app/pages/profile-page/profile-page.component.ts
@@ -4,6 +4,7 @@ import { Store } from '@ngrx/store';
 import { Subscription } from 'rxjs';
 import { AppState } from 'src/app/store/app.state';
 import { loadProfileDetails, selectProfileDetails } from 'src/app/store/profile.store';
+import { selectActiveProjectReports } from 'src/app/store/project-report.store';
 
 @Component({
   selector: 'app-profile-page',
@@ -14,6 +15,19 @@ export class ProfilePageComponent implements OnInit {
   selectProjectReportsSub: Subscription;
   profileDetails$ = this.store.select(selectProfileDetails);
   teamDetails$ = this.store.select(selectProfileDetails);
+  plantings$ = this.store.select(selectActiveProjectReports);
+  plantings: any[] = [
+    { name: 'name', amount: 2 },
+    { name: 'fiehra', amount: 21 },
+    { name: 'amount', amount: 23 },
+    { name: 'ring', amount: 22 },
+    { name: 'name', amount: 2 },
+    { name: 'fiehra', amount: 21 },
+    { name: 'amount', amount: 23 },
+    { name: 'ring', amount: 22 },
+  ];
+  // teamDetails$ = this.store.select(selectTeamDetails);
+  // plantings$ = this.store.select(selectPlantings);
 
   constructor(private store: Store<AppState>, private route: ActivatedRoute) {
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
@@ -25,5 +39,6 @@ export class ProfilePageComponent implements OnInit {
     this.profileDetails$.subscribe((res) => {
       console.log(res);
     });
+    console.log(this.plantings);
   }
 }
