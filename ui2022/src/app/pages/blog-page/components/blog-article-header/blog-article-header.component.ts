f0183bc3fa2fce0523627fdfd52b68daab24388c
tolga <tolga.schneider@thequalitygate.com>
2023-03-08 17:00:38 +0100

    refactored blog article layout

--- ui2022/src/app/pages/blog-page/components/blog-article-header/blog-article-header.component.ts
+++ ui2022/src/app/pages/blog-page/components/blog-article-header/blog-article-header.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { BlogArticle } from 'src/app/store/blog.store';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-blog-article-header',
@@ -8,8 +9,15 @@ import { BlogArticle } from 'src/app/store/blog.store';
 })
 export class BlogArticleHeaderComponent implements OnInit {
   @Input() blogArticle: BlogArticle;
+  imageUrl;
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.imageUrl =
+      environment.backendUrl +
+      '/article/image/' +
+      encodeURI(this.blogArticle.imageFileName) +
+      '/300/300';
+  }
 }
