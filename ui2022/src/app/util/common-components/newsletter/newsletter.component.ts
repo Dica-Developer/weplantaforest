0a882b4193ced61703e20ec2097c5e5bc6a9807b
tolga <tolga.schneider@thequalitygate.com>
2023-05-02 16:21:42 +0200

    added iframe for newsletter

--- ui2022/src/app/util/common-components/newsletter/newsletter.component.ts
+++ ui2022/src/app/util/common-components/newsletter/newsletter.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  SecurityContext,
+  ViewChild,
+} from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,7 +16,13 @@ import { Router } from '@angular/router';
 })
 export class NewsletterComponent implements OnInit {
   fullScreenNewsletter: boolean = false;
-  constructor(private router: Router) {}
+  html = this.sanitizer.bypassSecurityTrustHtml(
+    '<iframe width="100%" height="150" src="https://t924dfe8a.emailsys1a.net/126/2029/86f32163be/subscribe/form.html"></iframe>',
+  );
+  safeUrl;
+
+  @ViewChild('iframe') submitButton: ElementRef<HTMLElement>;
+  constructor(private router: Router, protected sanitizer: DomSanitizer) {}
 
   ngOnInit(): void {
     if (this.router.url.includes('newsletter')) {
