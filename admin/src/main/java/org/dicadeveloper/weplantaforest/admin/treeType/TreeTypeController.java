7c2136288e278d6fb8a3ab5e62c6e12ad3417e50
Gabor Barath <gabor_barath@yahoo.de>
2023-06-06 12:18:06 +0200

    trimmed imageName by spaces or / for treetypes

--- admin/src/main/java/org/dicadeveloper/weplantaforest/admin/treeType/TreeTypeController.java
+++ admin/src/main/java/org/dicadeveloper/weplantaforest/admin/treeType/TreeTypeController.java
@@ -26,83 +26,89 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class TreeTypeController {
 
-    private @NonNull TreeTypeRepository _treeTypeRepository;
+  private @NonNull TreeTypeRepository _treeTypeRepository;
 
-    private @NonNull ImageHelper _imageHelper;
+  private @NonNull ImageHelper _imageHelper;
 
-    @RequestMapping(value = Uris.TREETYPES, method = RequestMethod.GET)
-    public Iterable<TreeType> getTreeTypes() {
-        return _treeTypeRepository.findAll();
+  @RequestMapping(value = Uris.TREETYPES, method = RequestMethod.GET)
+  public Iterable<TreeType> getTreeTypes() {
+    return _treeTypeRepository.findAll();
+  }
+
+  @RequestMapping(value = Uris.TREETYPE_SAVE, method = RequestMethod.POST)
+  public ResponseEntity<?> saveTreeType(@RequestBody TreeType treeType) {
+    TreeType savedTreeType;
+    try {
+      savedTreeType = _treeTypeRepository.save(treeType);
+      return new ResponseEntity<>(savedTreeType.getId(), HttpStatus.OK);
+    } catch (Exception e) {
+      return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  @RequestMapping(value = Uris.TREETYPE_DELETE, method = RequestMethod.DELETE)
+  public ResponseEntity<?> deleteTreeType(@RequestParam Long TreeTypeId) {
+
+    try {
+      _treeTypeRepository.deleteById(TreeTypeId);
+      return new ResponseEntity<>(HttpStatus.OK);
+    } catch (Exception e) {
+      if (e instanceof DataIntegrityViolationException) {
+        String errorMessage = "Der Baumtyp kann nicht gelöscht werden, weil er bereits Bestandteil in Projekten ist.";
+        return new ResponseEntity<>(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR);
+      } else {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+      }
+    }
+  }
+
+  @RequestMapping(value = Uris.TREETYPE_IMAGE_UPLOAD, method = RequestMethod.POST)
+  @Transactional
+  public ResponseEntity<?> uploadUserImage(@RequestParam Long treeTypeId, @RequestParam String imageType,
+      @RequestParam("file") MultipartFile file) {
+    TreeType treeType = _treeTypeRepository.findById(treeTypeId).orElse(null);
+    String fileEnding;
+    if (file.getOriginalFilename().length() > 0) {
+      fileEnding = file.getOriginalFilename().substring(file.getOriginalFilename().indexOf("."));
+    } else {
+      fileEnding = ".png";
     }
 
-    @RequestMapping(value = Uris.TREETYPE_SAVE, method = RequestMethod.POST)
-    public ResponseEntity<?> saveTreeType(@RequestBody TreeType treeType) {
-        TreeType savedTreeType;
-        try {
-            savedTreeType = _treeTypeRepository.save(treeType);
-            return new ResponseEntity<>(savedTreeType.getId(), HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    String imageFolder = FileSystemInjector.getTreeTypeFolder();
+    String trimmedName = StringHelper.getTextForLanguage(treeType.getName(), "de");
+    int spaceIndex = trimmedName.indexOf(" ") != -1 ? trimmedName.indexOf(" ") : trimmedName.length();
+    trimmedName = trimmedName.substring(0, spaceIndex);
+    int slashIndex = trimmedName.indexOf("/") != -1 ? trimmedName.indexOf("/") : trimmedName.length();
+    trimmedName = trimmedName.substring(0, slashIndex);
+    String imageName = trimmedName + "_" + imageType + fileEnding;
+    System.out.println(imageName);
+    if (!file.isEmpty()) {
+      try {
+        imageName = _imageHelper.storeImage(file, imageFolder, imageName, true);
+        switch (imageType) {
+          case "treeImageColor":
+            treeType.setTreeImageColor(imageName);
+            break;
+          case "treeImageBW":
+            treeType.setTreeImageBW(imageName);
+            break;
+          case "fruitImageBW":
+            treeType.setFruitImageBW(imageName);
+            break;
+          case "fruitImageColor":
+            treeType.setFruitImageColor(imageName);
+            break;
+          default:
+            return new ResponseEntity<>("invalid treetype", HttpStatus.INTERNAL_SERVER_ERROR);
         }
+        _treeTypeRepository.save(treeType);
+        return new ResponseEntity<>(HttpStatus.OK);
+      } catch (IOException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+      }
+    } else {
+      return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
-
-    @RequestMapping(value = Uris.TREETYPE_DELETE, method = RequestMethod.DELETE)
-    public ResponseEntity<?> deleteTreeType(@RequestParam Long TreeTypeId) {
-
-        try {
-            _treeTypeRepository.deleteById(TreeTypeId);
-            return new ResponseEntity<>(HttpStatus.OK);
-        } catch (Exception e) {
-            if (e instanceof DataIntegrityViolationException) {
-                String errorMessage = "Der Baumtyp kann nicht gelöscht werden, weil er bereits Bestandteil in Projekten ist.";
-                return new ResponseEntity<>(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR);
-            } else {
-                return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
-    }
-
-    @RequestMapping(value = Uris.TREETYPE_IMAGE_UPLOAD, method = RequestMethod.POST)
-    @Transactional
-    public ResponseEntity<?> uploadUserImage(@RequestParam Long treeTypeId, @RequestParam String imageType ,@RequestParam("file") MultipartFile file) {
-        TreeType treeType = _treeTypeRepository.findById(treeTypeId).orElse(null);
-        String fileEnding;
-        if (file.getOriginalFilename().length() > 0) {
-            fileEnding = file.getOriginalFilename().substring(file.getOriginalFilename().indexOf("."));
-        } else {
-            fileEnding = ".png";
-        }
-
-        String imageFolder = FileSystemInjector.getTreeTypeFolder();
-        String imageName = StringHelper.getTextForLanguage(treeType.getName(), "de") + "_" + imageType + fileEnding ;
-
-        if (!file.isEmpty()) {
-            try {
-                imageName = _imageHelper.storeImage(file, imageFolder, imageName, true);
-                switch(imageType) {
-                  case "treeImageColor":
-                    treeType.setTreeImageColor(imageName);
-                    break;
-                  case "treeImageBW":
-                    treeType.setTreeImageBW(imageName);
-                    break;
-                  case "fruitImageBW":
-                    treeType.setFruitImageBW(imageName);
-                    break;
-                  case "fruitImageColor":
-                    treeType.setFruitImageColor(imageName);
-                    break;
-                  default:
-                    return new ResponseEntity<>("invalid treetype", HttpStatus.INTERNAL_SERVER_ERROR);
-                }
-                _treeTypeRepository.save(treeType);
-                return new ResponseEntity<>(HttpStatus.OK);
-            } catch (IOException e) {
-                return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        } else {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-    }
+  }
 
 }
