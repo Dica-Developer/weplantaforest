9d309462f3ba139a490fdd92e2e83a19c664c227
Gabor <gabor.barath@lsc-gmbh.net>
2023-10-17 13:35:38 +0200

    created BehaviorSubjects for map and projectAreas in leaflet map to signalize when each is loaded
    * combined them in CombinLatest SUbscription and only start creating polygons and markers if both are loaded
    * fixed #655

--- ui2022/src/app/util/common-components/leaflet-map/leaflet-map.component.ts
+++ ui2022/src/app/util/common-components/leaflet-map/leaflet-map.component.ts
@@ -1,14 +1,23 @@
-import { Component, EventEmitter, HostListener, Input, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  HostListener,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
 import { UntypedFormControl } from '@angular/forms';
 import * as L from 'leaflet';
 import { tileLayer, marker, icon, Map } from 'leaflet';
+import { BehaviorSubject, combineLatest, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-leaflet-map',
   templateUrl: './leaflet-map.component.html',
   styleUrls: ['./leaflet-map.component.scss'],
 })
-export class LeafletMapComponent implements OnInit {
+export class LeafletMapComponent implements OnInit, OnDestroy {
   map: Map;
   coords = [];
   polygon;
@@ -20,19 +29,16 @@ export class LeafletMapComponent implements OnInit {
   @Input() mapHeight: string = '600px';
   @Output() markerSet = new EventEmitter();
 
+  mapSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  projectAreasSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  combinedSub: Subscription;
+
+  projectAreasInternal: any[][];
+
   @Input('projectAreas')
   set projectAreas(areas: any[][]) {
-    this.treeMarker = [];
-    for (let area of areas) {
-      const coords = this.createPolygonPoints(area);
-      const polygon = L.polygon(coords, { color: '#82ab1f' });
-      this.map.addLayer(polygon);
-      const marker = this.createMarker(polygon.getCenter().lat, polygon.getCenter().lng);
-      marker.on('click', (event) => {
-        event.target._map.setView(event.latlng, 14);
-      });
-      this.treeMarker.push(marker);
-    }
+    this.projectAreasInternal = areas;
+    this.projectAreasSubject.next(true);
   }
 
   treeMarker: any[];
@@ -58,12 +64,34 @@ export class LeafletMapComponent implements OnInit {
     if (this.positions) {
       this.coords = this.createPolygonPoints(this.positions);
     }
+
+    this.combinedSub = combineLatest([this.mapSubject, this.projectAreasSubject]).subscribe(
+      (res) => {
+        if (res[0] && res[1]) {
+          // map and area points are loaded, now start adding marker and polygons
+          this.treeMarker = [];
+          for (let area of this.projectAreasInternal) {
+            const coords = this.createPolygonPoints(area);
+            const polygon = L.polygon(coords, { color: '#82ab1f' });
+            this.map.addLayer(polygon);
+            const marker = this.createMarker(polygon.getCenter().lat, polygon.getCenter().lng);
+            marker.on('click', (event) => {
+              event.target._map.setView(event.latlng, 14);
+            });
+            this.treeMarker.push(marker);
+          }
+        }
+      },
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.combinedSub?.unsubscribe();
   }
 
   onMapReady(map: Map): void {
     this.map = map;
     this.map.scrollWheelZoom.disable();
-
     setTimeout(() => {
       if (this.coords.length > 0) {
         this.polygon = L.polygon(this.coords, { color: '#82ab1f' });
@@ -78,6 +106,7 @@ export class LeafletMapComponent implements OnInit {
         }
       }
       map.invalidateSize();
+      this.mapSubject.next(true);
     });
   }
 
@@ -106,12 +135,8 @@ export class LeafletMapComponent implements OnInit {
     return marker([lat, lng], {
       icon: icon({
         iconAnchor: [17, 35],
-        // iconUrl: '/assets/treeIconGreen.png',
-        // iconRetinaUrl: '/assets/treeIconGreen.png',
         iconUrl: '/assets/treeIconBrown.png',
         iconRetinaUrl: '/assets/treeIconBrown.png',
-        // iconUrl: '/assets/treeIcon.png',
-        // iconRetinaUrl: '/assets/treeIcon.png',
       }),
     });
   }
