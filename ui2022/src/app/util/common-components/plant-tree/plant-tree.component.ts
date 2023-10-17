73ea7f27b5a5250bb8f1553880c91ea0f1cf906c
Gabor <gabor.barath@lsc-gmbh.net>
2023-10-17 12:59:37 +0200

    fixed #588

--- ui2022/src/app/util/common-components/plant-tree/plant-tree.component.ts
+++ ui2022/src/app/util/common-components/plant-tree/plant-tree.component.ts
@@ -6,6 +6,8 @@ import { selectProjectsForCustomPlanting } from '../../../store/plant.store';
 import { environment } from '../../../../environments/environment';
 import { ProfileDetails, selectProfileDetails } from '../../../store/profile.store';
 import { TextHelper } from '../../text.helper';
+import { ActiveProjectArticle } from '../../../store/project.store';
+import { addPlantbagItem, resetPlantbag } from '../../../store/plantbag.store';
 
 @Component({
   selector: 'app-plant-tree',
@@ -14,7 +16,7 @@ import { TextHelper } from '../../text.helper';
 })
 export class PlantTreeComponent implements OnInit, OnDestroy {
   hover: boolean;
-  trees: { name: string; urlColor: string; urlBW: string }[] = [];
+  trees: { article: ActiveProjectArticle; name: string; urlColor: string; urlBW: string }[] = [];
 
   activeProjects$: Observable<any> = this.store.select(selectProjectsForCustomPlanting);
   profileDetails$: Observable<ProfileDetails> = this.store.select(selectProfileDetails);
@@ -29,28 +31,35 @@ export class PlantTreeComponent implements OnInit, OnDestroy {
         this.trees = [];
         if (activeProjects && activeProjects.length > 0) {
           for (const project of activeProjects) {
-            if (project.articles?.length > 0) {
-              for (const article of project.articles) {
-                if (this.trees.length < 3) {
-                  this.trees.push({
-                    name: this.textHelper.getTextForLanguage(
-                      article.treeType.name,
-                      profileDetails?.lang ?? 'de',
-                    ),
-                    urlColor:
-                      environment.backendUrl +
-                      '/treeType/image/' +
-                      article.treeType.treeImageColor +
-                      '/1000/1000',
-                    urlBW:
-                      environment.backendUrl +
-                      '/treeType/image/' +
-                      article.treeType.treeImageBW +
-                      '/1000/1000',
-                  });
-                }
-              }
+            if (!project.articles || project.articles.length === 0) {
+              // only start creating trees if there are articles laoded in every project
+              return;
             }
+          }
+          const projectCount = activeProjects.length;
+
+          for (let i = 0; i < 3; i++) {
+            const randomProjectIndex = Math.floor(Math.random() * projectCount);
+            const articleCount = activeProjects[randomProjectIndex].articles.length;
+            const randomArticleIndex = Math.floor(Math.random() * articleCount);
+            const article = activeProjects[randomProjectIndex].articles[randomArticleIndex];
+            this.trees.push({
+              article,
+              name: this.textHelper.getTextForLanguage(
+                article.treeType.name,
+                profileDetails?.lang ?? 'de',
+              ),
+              urlColor:
+                environment.backendUrl +
+                '/treeType/image/' +
+                article.treeType.treeImageColor +
+                '/1000/1000',
+              urlBW:
+                environment.backendUrl +
+                '/treeType/image/' +
+                article.treeType.treeImageBW +
+                '/1000/1000',
+            });
           }
         }
       },
@@ -59,5 +68,16 @@ export class PlantTreeComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.combinedSub?.unsubscribe();
+  }
+
+  addTreeToPlantBag(tree: {
+    article: ActiveProjectArticle;
+    name: string;
+    urlColor: string;
+    urlBW: string;
+  }) {
+    this.store.dispatch(resetPlantbag());
+    const item = { article: tree.article, amount: 5 };
+    this.store.dispatch(addPlantbagItem({ item }));
   }
 }
