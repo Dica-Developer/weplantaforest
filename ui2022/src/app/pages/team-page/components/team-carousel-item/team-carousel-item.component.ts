12b1ade579471a1ff586593a94bb777f068c9280
Gabor <gabor.barath@lsc-gmbh.net>
2023-11-01 11:32:22 +0100

    fixed #700

--- ui2022/src/app/pages/team-page/components/team-carousel-item/team-carousel-item.component.ts
+++ ui2022/src/app/pages/team-page/components/team-carousel-item/team-carousel-item.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { TeamMember } from 'src/app/store/team.store';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-team-carousel-item',
@@ -7,9 +9,25 @@ import { TeamMember } from 'src/app/store/team.store';
   styleUrls: ['./team-carousel-item.component.scss'],
 })
 export class TeamCarouselItemComponent implements OnInit {
-  @Input() member: TeamMember;
+  _member: TeamMember;
+  imageUrl: string;
 
-  constructor() {}
+  @Input()
+  set member(member: TeamMember) {
+    this._member = member;
+    if (member.imageName) {
+      this.imageUrl = `${environment.backendUrl}/user/image/${member.imageName}/60/60`;
+    } else {
+      this.imageUrl = 'assets/images/user.png';
+    }
+  }
+
+  constructor(private router: Router) {}
 
   ngOnInit(): void {}
+
+  routeToProifile() {
+
+    this.router.navigate(['/profile', this._member.name]);
+  }
 }
