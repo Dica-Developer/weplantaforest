f5dc90c1de0257fa84d39433fe5a68d7e06a6277
Gabor <gabor.barath@lsc-gmbh.net>
2023-11-14 10:57:18 +0100

    fixed #685

--- ui2022/src/app/pages/plant-proposal-page/components/custom-planting/custom-planting.component.ts
+++ ui2022/src/app/pages/plant-proposal-page/components/custom-planting/custom-planting.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Subscription } from 'rxjs';
+import { combineLatest, Subscription } from 'rxjs';
 import {
   getProjectsForCustomPlanting,
   selectProjectsForCustomPlanting,
+  selectSimpleProposal,
 } from '../../../../store/plant.store';
 import { selectProfileDetails } from '../../../../store/profile.store';
 import { AppState } from '../../../../store/app.state';
@@ -23,6 +24,7 @@ export class CustomPlantingComponent implements OnInit, OnDestroy {
   profileDetailsSub: Subscription;
   profileDetails;
   language$ = this.store.select(selectProfileDetails);
+  proposalSub: Subscription;
 
   @Input()
   projectId: number;
@@ -34,47 +36,29 @@ export class CustomPlantingComponent implements OnInit, OnDestroy {
 
   totalPrice: string = '0,00';
   formGroupSub: Subscription;
+  combinedSub: Subscription;
 
   constructor(private store: Store<AppState>, private textHelper: TextHelper) {}
 
   ngOnInit(): void {
-    this.activeProjectsSub = this.store
-      .select(selectProjectsForCustomPlanting)
-      .subscribe((projects) => {
-        this.activeProjects = projects;
-        if (this.projectId) {
-          this.activeProjects = projects.filter((project) => project.projectId === this.projectId);
-        }
-        this.formGroup = new FormGroup({});
-        for (const project of this.activeProjects) {
-          this.formGroup.addControl(project.projectName, new FormArray([]));
-          if (project.articles && project.articles.length > 0) {
-            for (const article of project.articles) {
-              (this.formGroup.get(project.projectName) as FormArray).push(
-                new FormGroup({
-                  article: new FormControl(article),
-                  amount: new FormControl(0, [
-                    Validators.min(0),
-                    Validators.max(article.amount - article.alreadyPlanted),
-                  ]),
-                }),
-              );
-            }
-            this.formGroupSub?.unsubscribe();
-            this.formGroupSub = this.formGroup.valueChanges.subscribe((value) => {
-              let price = 0;
-              for (const project of Object.keys(value)) {
-                for (const article of value[project]) {
-                  price += (article.amount * article.article.price.priceAsLong) / 100;
-                }
+    this.combinedSub = combineLatest([
+      this.store.select(selectSimpleProposal),
+      this.store.select(selectProjectsForCustomPlanting),
+    ]).subscribe(([proposal, projects]) => {
+      this.initForm(projects);
+      if (proposal) {
+        for (const plantItem of proposal.plantItems) {
+          if((this.formGroup.get(plantItem.projectName) as FormArray)) {
+            for (const fg of (this.formGroup.get(plantItem.projectName) as FormArray).controls) {
+              if (fg.value.article.treeType.name === plantItem.treeType) {
+                fg.get('amount').setValue(plantItem.amount);
               }
-              this.totalPrice = price.toFixed(2).replace('.', ',');
-              this.showPutIntoPlantbagButton = true;
-              this.showGoToPlantbagButton = false;
-            });
+            }
           }
         }
-      });
+      }
+    });
+
     this.profileDetailsSub = this.store.select(selectProfileDetails).subscribe((details) => {
       this.profileDetails = details;
     });
@@ -115,5 +99,42 @@ export class CustomPlantingComponent implements OnInit, OnDestroy {
 
   getFormArray(name: string) {
     return (this.formGroup.get(name) as FormArray).controls;
+  }
+
+  initForm(projects) {
+    this.activeProjects = projects;
+    if (this.projectId) {
+      this.activeProjects = projects.filter((project) => project.projectId === this.projectId);
+    }
+    this.formGroup = new FormGroup({});
+    for (const project of this.activeProjects) {
+      this.formGroup.addControl(project.projectName, new FormArray([]));
+      if (project.articles && project.articles.length > 0) {
+        for (const article of project.articles) {
+          (this.formGroup.get(project.projectName) as FormArray).push(
+            new FormGroup({
+              article: new FormControl(article),
+              amount: new FormControl(0, [
+                Validators.min(0),
+                Validators.max(article.amount - article.alreadyPlanted),
+              ]),
+            }),
+          );
+        }
+      }
+    }
+
+    this.formGroupSub?.unsubscribe();
+    this.formGroupSub = this.formGroup.valueChanges.subscribe((value) => {
+      let price = 0;
+      for (const project of Object.keys(value)) {
+        for (const article of value[project]) {
+          price += (article.amount * article.article.price.priceAsLong) / 100;
+        }
+      }
+      this.totalPrice = price.toFixed(2).replace('.', ',');
+      this.showPutIntoPlantbagButton = true;
+      this.showGoToPlantbagButton = false;
+    });
   }
 }
