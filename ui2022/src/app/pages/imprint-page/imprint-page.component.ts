eab9e0f6ef56fad9be1cb585d3b02578fea479f0
Gabor <gabor.barath@lsc-gmbh.net>
2023-11-21 14:25:30 +0100

    fixed issue with incorrect language on imprint page

--- ui2022/src/app/pages/imprint-page/imprint-page.component.ts
+++ ui2022/src/app/pages/imprint-page/imprint-page.component.ts
@@ -1,24 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { ContentService } from 'src/app/services/content.service';
 import { TextHelper } from 'src/app/util/text.helper';
+import { Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/store/app.state';
+import { selectUserLanguage } from '../../store/profile.store';
 
 @Component({
   selector: 'app-imprint-page',
   templateUrl: './imprint-page.component.html',
   styleUrls: ['./imprint-page.component.scss'],
 })
-export class ImprintPageComponent implements OnInit {
+export class ImprintPageComponent implements OnInit, OnDestroy {
   imprints;
 
-  constructor(private textHelper: TextHelper, private contentService: ContentService) {}
+  imprintSub: Subscription;
+  languageSub: Subscription;
+
+  constructor(private store: Store<AppState>, private contentService: ContentService) {}
 
   ngOnInit(): void {
-    this.textHelper.getCurrentLanguage();
-    this.contentService
-      .getInfrastructureArticle('IMPRESS', this.textHelper.getCurrentLanguage())
-      .subscribe((res) => {
-        this.imprints = res;
-      });
+    this.languageSub = this.store.select(selectUserLanguage).subscribe((language) => {
+      this.imprintSub?.unsubscribe();
+      let lng = '';
+      if (language === 'ENGLISH' || language === 'en') {
+        lng = 'ENGLISH';
+      } else if (language === 'DEUTSCH' || language === 'de') {
+        lng = 'DEUTSCH';
+      }
+      this.imprintSub = this.contentService
+        .getInfrastructureArticle('IMPRESS', lng)
+        .subscribe((res) => {
+          this.imprints = res;
+        });
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.imprintSub?.unsubscribe();
+    this.languageSub?.unsubscribe();
   }
 }
