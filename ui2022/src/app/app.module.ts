ab3a6ff698b7a57eba21630086fa0cbdf31530ba
tolga <tolga.schneider@thequalitygate.com>
2023-12-15 02:12:03 +0100

    removed ngx editor module from package json

--- ui2022/src/app/app.module.ts
+++ ui2022/src/app/app.module.ts
@@ -21,7 +21,6 @@ import {
 import { AgGridModule } from 'ag-grid-angular';
 import { userReducerFn, UserEffects } from './store/user.store';
 import { projectsReducerFn, ProjectsEffects } from './store/project.store';
-import { NgxEditorModule } from 'ngx-editor';
 import { FormsModule } from '@angular/forms';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { LeafletDrawModule } from '@asymmetrik/ngx-leaflet-draw';
@@ -137,41 +136,6 @@ registerLocaleData(localeEn, 'en-EN', localeEnExtra);
     MatNativeDateModule,
     AgGridModule.forRoot(),
     FormsModule,
-    NgxEditorModule.forRoot({
-      locals: {
-        // menu
-        bold: 'Bold',
-        italic: 'Italic',
-        code: 'Code',
-        blockquote: 'Blockquote',
-        underline: 'Underline',
-        strike: 'Strike',
-        bullet_list: 'Bullet List',
-        ordered_list: 'Ordered List',
-        heading: 'Heading',
-        h1: 'Header 1',
-        h2: 'Header 2',
-        h3: 'Header 3',
-        h4: 'Header 4',
-        h5: 'Header 5',
-        h6: 'Header 6',
-        align_left: 'Left Align',
-        align_center: 'Center Align',
-        align_right: 'Right Align',
-        align_justify: 'Justify',
-        text_color: 'Text Color',
-        background_color: 'Background Color',
-
-        // popups, forms, others...
-        url: 'URL',
-        text: 'Text',
-        openInNewTab: 'Open in new tab',
-        insert: 'Insert',
-        altText: 'Alt Text',
-        title: 'Title',
-        remove: 'Remove',
-      },
-    }),
     LeafletModule,
     LeafletDrawModule,
     TranslateModule.forRoot({
