342973c9e5f85b5f231658d4333b503f01bba1aa
Gabor <gabor.barath@lsc-gmbh.net>
2024-01-06 12:16:55 +0100

    removed base href url from prod deplyoment

--- ui2022/deploy-prod.sh
+++ ui2022/deploy-prod.sh
@@ -4,23 +4,23 @@
 set -e
 
 # replace font urls with base href prefix '/ui2022'
-find='assets';
-replace='ui2022/assets';
-find src/assets/fonts/bull5/bull5.css -type f -exec sed -i "s#$find#$replace#g" {} +
-find src/assets/fonts/nbArchitect/nbArchitect.css -type f -exec sed -i "s#$find#$replace#g" {} +
+# find='assets';
+# replace='ui2022/assets';
+# find src/assets/fonts/bull5/bull5.css -type f -exec sed -i "s#$find#$replace#g" {} +
+# find src/assets/fonts/nbArchitect/nbArchitect.css -type f -exec sed -i "s#$find#$replace#g" {} +
 
 # clean dist
 rm -rf dist
 # install dependencies
 yarn install
 # build app
-yarn build --configuration=prod --base-href /ui2022/ --deploy-url /ui2022/
+yarn build --configuration=prod
 
 # undo find/replace changes
-find='ui2022/assets';
-replace='assets';
-find src/assets/fonts/bull5/bull5.css -type f -exec sed -i "s#$find#$replace#g" {} +
-find src/assets/fonts/nbArchitect/nbArchitect.css -type f -exec sed -i "s#$find#$replace#g" {} +
+# find='ui2022/assets';
+# replace='assets';
+# find src/assets/fonts/bull5/bull5.css -type f -exec sed -i "s#$find#$replace#g" {} +
+# find src/assets/fonts/nbArchitect/nbArchitect.css -type f -exec sed -i "s#$find#$replace#g" {} +
 
 # create ui backup
 ssh ipat@iplantatree.org "rm -r iplantatree/ui2022.backup/*"
