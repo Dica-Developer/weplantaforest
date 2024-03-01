dc80df57f7613845e00e01860c8a1f4c9b4f8c3b
fiehra <switchedspace@gmail.com>
2024-03-01 02:20:56 +0100

    added missing image code to html and set image urls in TS file

--- ui2022/src/app/pages/about-page/about-page.component.ts
+++ ui2022/src/app/pages/about-page/about-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { ContentService } from 'src/app/services/content.service';
 import { TextHelper } from 'src/app/util/text.helper';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-about-page',
@@ -11,14 +12,19 @@ import { TextHelper } from 'src/app/util/text.helper';
 export class AboutPageComponent implements OnInit {
   lang: string;
   aboutUs;
-
+  mainImageUrl: string;
+  paragraphImgUrls: string[] = [];
   constructor(private textHelper: TextHelper, private contentService: ContentService) {}
 
   ngOnInit(): void {
     this.contentService
       .getInfrastructureArticle('ABOUT_US', this.textHelper.getCurrentLanguage())
       .subscribe((res) => {
+        this.mainImageUrl = environment.backendArticleManagerUrl + '/article/image/' + res[0].id + '/' + encodeURI(res[0].imageFileName);
         this.aboutUs = res;
+        for (let i = 0; i < this.aboutUs[0].paragraphs.length; i++) {
+          this.paragraphImgUrls.push(environment.backendArticleManagerUrl + '/article/image/' + res[0].id + '/' + encodeURI(this.aboutUs[0].paragraphs[i].imageFileName));
+        }
       });
   }
 }
