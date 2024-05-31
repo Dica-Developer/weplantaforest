9eaaf012571266e3220a6cb15f5dc929fbf599cc
fiehra <switchedspace@gmail.com>
2024-05-31 22:13:57 +0200

    moved helper files

--- ui2022/src/app/services/leaflet.service.ts
+++ ui2022/src/app/services/leaflet.service.ts
@@ -4,6 +4,11 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class LeafletService {
+  isBrowser: boolean;
+
+  this.isBrowser = isPlatformBrowser(this.platformId);
+
+
   constructor() {}
 
 }
