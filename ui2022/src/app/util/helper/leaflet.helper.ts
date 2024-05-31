a68f3269e4b2b2accefa205ba37ffb3727a72470
fiehra <switchedspace@gmail.com>
2024-05-31 22:39:40 +0200

    added leaflet helper

---
+++ ui2022/src/app/util/helper/leaflet.helper.ts
@@ -0,0 +1,16 @@
+
+import { isPlatformBrowser } from '@angular/common';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LeafletService {
+  isBrowser: boolean;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: any) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+  }
+
+
+}
