889d4f69b30a5f875c0d9ed7ca4cf2a57a5cf8d7
fiehra <switchedspace@gmail.com>
2024-06-27 14:55:43 +0200

    added newsletter page to refactor cookies

---
+++ ui2022/src/app/pages/newsletter-page/newsletter-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-newsletter-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './newsletter-page.component.html',
+  styleUrl: './newsletter-page.component.scss'
+})
+export class NewsletterPageComponent {
+
+}
