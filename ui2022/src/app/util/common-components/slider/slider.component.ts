815b4acd6186eef82bc203de55040c13e2f8e7e7
fiehra <switchedspace@gmail.com>
2024-07-22 15:30:36 +0200

    added self reliant slider component

---
+++ ui2022/src/app/util/common-components/slider/slider.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-slider',
+  standalone: true,
+  imports: [],
+  templateUrl: './slider.component.html',
+  styleUrl: './slider.component.scss'
+})
+export class SliderComponent {
+
+}
