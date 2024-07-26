5f04e3a14b3b04317e6729d74a57c09739e90cad
fiehra <switchedspace@gmail.com>
2024-07-26 11:22:40 +0200

    debugged rankings and added router link

--- ui2022/src/app/pages/home-page/components/rankings/rankings.component.ts
+++ ui2022/src/app/pages/home-page/components/rankings/rankings.component.ts
@@ -1,10 +1,13 @@
 import { AsyncPipe, NgFor, TitleCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+import { MatTooltip } from '@angular/material/tooltip';
+import { RouterLink } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { TranslateModule } from '@ngx-translate/core';
 import { RankingItemComponent } from 'src/app/pages/ranking-page/components/ranking-item/ranking-item.component';
 import { AppState } from 'src/app/store/app.state';
-import { loadRankings, selectRankings } from 'src/app/store/ranking.store';
+import { loadRankings, selectRankingMaxAmount, selectRankings } from 'src/app/store/ranking.store';
 
 @Component({
   selector: 'app-rankings',
@@ -12,6 +15,9 @@ import { loadRankings, selectRankings } from 'src/app/store/ranking.store';
   imports: [
     NgFor,
     TranslateModule,
+    RouterLink,
+    MatTooltip,
+    MatIcon,
     TitleCasePipe,
     AsyncPipe,
     RankingItemComponent
@@ -21,6 +27,7 @@ import { loadRankings, selectRankings } from 'src/app/store/ranking.store';
 })
 export class RankingsComponent {
   rankings$ = this.store.select(selectRankings);
+  maxValue$ = this.store.select(selectRankingMaxAmount);
 
   constructor(
     private store: Store<AppState>) {
