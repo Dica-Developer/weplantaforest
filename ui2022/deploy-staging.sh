d30aec6f5f302502d472dcd3e97209ca19deb737
Gabor <gabor.barath@lsc-gmbh.net>
2024-09-28 17:19:40 +0200

    1st approach about ssr frontend and dockerized environment

--- ui2022/deploy-staging.sh
+++ ui2022/deploy-staging.sh
@@ -23,10 +23,10 @@ find src/assets/fonts/bull5/bull5.css -type f -exec sed -i "s#$find#$replace#g" 
 find src/assets/fonts/nbArchitect/nbArchitect.css -type f -exec sed -i "s#$find#$replace#g" {} +
 
 # create ui backup
-ssh ipat@iplantatree.org "rm -r iplantatree/staging.backup/*"
-ssh ipat@iplantatree.org "cp -a  iplantatree/ui/dist/staging/. iplantatree/staging.backup/ "
+ssh -oHostKeyAlgorithms=+ssh-dss ipat@iplantatree.org "rm -r iplantatree/staging.backup/*"
+ssh -oHostKeyAlgorithms=+ssh-dss ipat@iplantatree.org "cp -a  iplantatree/ui/dist/staging/. iplantatree/staging.backup/ "
 # clean remote folder first
-ssh ipat@iplantatree.org "rm -r iplantatree/ui/dist/staging/*"
+ssh -oHostKeyAlgorithms=+ssh-dss ipat@iplantatree.org "rm -r iplantatree/ui/dist/staging/*"
 # deploy new version to server
-scp -r /home/ipat/weplantaforest/ui2022/dist/ui2022/* ipat@iplantatree.org:/home/ipat/iplantatree/ui/dist/staging
+scp -r /home/timelean/projekte/weplantaforest/ui2022/dist/ui2022/* ipat@iplantatree.org:/home/ipat/iplantatree/ui/dist/staging
 
