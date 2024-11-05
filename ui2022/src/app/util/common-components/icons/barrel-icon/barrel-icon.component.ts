2d18841dfe1c4a066384b82dbc286db3735e6d33
fiehra <switchedspace@gmail.com>
2024-11-05 14:19:20 +0100

    fixed styles updated logo in backoffice

--- ui2022/src/app/util/common-components/icons/barrel-icon/barrel-icon.component.ts
+++ ui2022/src/app/util/common-components/icons/barrel-icon/barrel-icon.component.ts
@@ -1,20 +1,16 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { PlatformHelper } from 'src/app/util/helper/platform.helper';
 
 @Component({
-  selector: 'app-barrel-icon',
-  templateUrl: './barrel-icon.component.html',
-  styleUrls: ['./barrel-icon.component.scss'],
-  standalone: true,
+    selector: 'app-barrel-icon',
+    templateUrl: './barrel-icon.component.html',
+    styleUrls: ['./barrel-icon.component.scss'],
+    standalone: true,
 })
 export class BarrelIconComponent implements OnInit {
   @Input() inToolbar: Boolean = false;
 
-  constructor(private platformHelper: PlatformHelper) {}
+  constructor() {}
 
   ngOnInit(): void {
-    if (this.inToolbar && this.platformHelper.checkIfBrowser()) {
-      document.getElementById('svg-icon').classList.add('inToolbar');
-    }
   }
 }
