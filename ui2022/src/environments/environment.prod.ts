0f168fb373f118a423fb1c6ca4412c055641074c
Gabor <gabor.barath@lsc-gmbh.net>
2024-11-11 15:29:10 +0100

    added stuff for production docker stuff

--- ui2022/src/environments/environment.prod.ts
+++ ui2022/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  backendUrl: 'https://iplantatree.org/p/u',
-  backendAdminUrl: 'https://iplantatree.org/p/a',
-  backendArticleManagerUrl: 'https://iplantatree.org/p/m',
+  backendUrl: 'https://iplantatree.org/u',
+  backendAdminUrl: 'https://iplantatree.org/a',
+  backendArticleManagerUrl: 'https://iplantatree.org/am',
   oldFrontendUrl: 'https://iplantatree.org',
   frontendUrl: 'https://iplantatree.org',
   baseUrl: ''
