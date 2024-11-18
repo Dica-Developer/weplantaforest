723d37d9a1738749c745b2502b34c66db7385ca5
fiehra <switchedspace@gmail.com>
2024-11-18 19:45:55 +0100

    removed binary from userRepo findByName to fix login with special characters

--- user/src/main/java/org/dicadeveloper/weplantaforest/user/UserRepository.java
+++ user/src/main/java/org/dicadeveloper/weplantaforest/user/UserRepository.java
@@ -27,7 +27,7 @@ public interface UserRepository extends CrudRepository<User, Long> {
 
     public static final String GET_ALL_TEAM_MEMBER = "Select user FROM User user WHERE user.team.id = :teamId";
 
-    @Query(value = "SELECT * FROM User user WHERE BINARY _name = :name", nativeQuery = true)
+    @Query(value = "SELECT * FROM User user WHERE _name = :name", nativeQuery = true)
     public User findByName(@Param("name") String name);
 
     @Query(value = USER_EXISTS_QUERY, nativeQuery = true)
