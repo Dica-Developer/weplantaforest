21f677fbe46af0c81005e44cfc8485af65574acb
fiehra <switchedspace@gmail.com>
2024-11-24 03:08:22 +0100

    fixed observable check in toolbar on mobile view

--- ui2022/src/app/services/statistics.service.ts
+++ ui2022/src/app/services/statistics.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { ContactRequest } from '../store/contact.store';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -9,19 +10,19 @@ import { ContactRequest } from '../store/contact.store';
 export class StatisticsService {
   constructor(private http: HttpClient) {}
 
-  getTreesPerYear() {
+  getTreesPerYear():Observable<any> {
     return this.http.get(environment.backendUrl + '/statistic/treesPerYear');
   }
 
-  getCo2() {
+  getCo2():Observable<any>{
     return this.http.get(environment.backendUrl + '/statistic/co2');
   }
 
-  getUsersPerYear() {
+  getUsersPerYear():Observable<any> {
     return this.http.get(environment.backendUrl + '/statistic/userPerYear');
   }
 
-  getTreesPerOrgType() {
+  getTreesPerOrgType():Observable<any> {
     return this.http.get(environment.backendUrl + '/statistic/treesPerOrgType');
   }
 }
